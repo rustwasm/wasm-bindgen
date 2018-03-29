c525ecffc2d9c5216f1564c64dadc7e87237f880
Ashley Williams <ashley666ashley@gmail.com>
2018-03-29 22:29:39 +0200

    feat(test): add a failing math test

--- tests/node.rs
+++ tests/node.rs
@@ -54,6 +54,61 @@ fn works() {
                     Color::Red => Color::Green,
                 }
             }
+
+            #[wasm_bindgen]
+            pub fn math(a: f32, b: f64) -> f64 {
+                b.acos() +
+                    b.asin() +
+                    b.atan() +
+                    b.atan2(b) +
+                    b.cbrt() +
+                    b.cosh() +
+                    b.exp_m1() +
+                    b.ln_1p() +
+                    b.sinh() +
+                    b.tan() +
+                    b.tanh() +
+                    b.hypot(b) +
+                    b.cos() +
+                    b.exp() +
+                    b.exp2() +
+                    b.mul_add(b, b) +
+                    b.ln() +
+                    b.log(b) +
+                    b.log10() +
+                    b.log2() +
+                    b.powi(8) +
+                    b.powf(b) +
+                    b.round() +
+                    b.sin() +
+                    b.abs() +
+                    b.signum() +
+                    b.floor() +
+                    b.ceil() +
+                    b.trunc() +
+                    b.sqrt() +
+                    (b % (a as f64)) +
+                    ((a.cos() +
+                    a.exp() +
+                    a.exp2() +
+                    a.mul_add(a, a) +
+                    a.ln() +
+                    a.log(a) +
+                    a.log10() +
+                    a.log2() +
+                    a.powi(8) +
+                    a.powf(a) +
+                    a.round() +
+                    a.sin() +
+                    a.abs() +
+                    a.signum() +
+                    a.floor() +
+                    a.ceil() +
+                    a.trunc() +
+                    a.sqrt() +
+                    (a % (b as f32))) as f64) +
+                    (b + 2.0f64.powf(a as f64))
+            }
         "#)
         .file("test.js", r#"
             const assert = require('assert');
@@ -91,6 +146,8 @@ fn works() {
                 assert.strictEqual(Color.Red, 2);
                 assert.strictEqual(Object.keys(Color).length, 3);
                 assert.strictEqual(Color.cycle(Color.Green), Color.Yellow);
+
+                math(1.0, 2.0);
             };
 
         "#)
