6a6be7ef889c2332bfaa70ea0279ebddc5072bd3
robert masen <r@robertmasen.pizza>
2018-04-23 11:32:57 -0500

    update comment to be more descriptive

--- examples/hello_world/chrome/build_fetch.sh
+++ examples/hello_world/chrome/build_fetch.sh
@@ -2,7 +2,10 @@
 
 set -ex
 
-# This is the same as the directory above this.
+# This is the same build.sh, later we are going to use the
+# fetch flag to avoid including the wasm module as a string
+# of base64, instead using the js `fetch` function 
+#to request the module from the server.
 cargo +nightly build --target wasm32-unknown-unknown
 cargo +nightly run -p wasm-bindgen-cli --bin wasm-bindgen -- \
   ../../../target/wasm32-unknown-unknown/debug/hello_world.wasm --out-dir .
@@ -12,7 +15,7 @@ cargo +nightly run -p wasm-bindgen-cli --bin wasm-bindgen -- \
 cargo +nightly run -p wasm-bindgen-cli --bin wasm2es6js -- \
   --fetch ./hello_world_bg.wasm -o hello_world_bg.js hello_world_bg.wasm
 
-# And like the directory above this, from here it's the same.
+# Install the npm items as usual.
 npm install
 
 # since we kept the same name for the .js module, we need
