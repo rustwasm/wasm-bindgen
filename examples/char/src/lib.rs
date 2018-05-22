e7e44e3fb058c4b2fbb73f6c092c7bd98e1c2c6b
Robert Masen <r@robertmasen.pizza>
2018-05-21 22:03:42 -0500

    add char test

---
+++ examples/char/src/lib.rs
@@ -0,0 +1,55 @@
+#![feature(proc_macro, wasm_custom_section, wasm_import_module)]
+
+extern crate wasm_bindgen;
+
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+extern {
+    #[wasm_bindgen(js_namespace = console)]
+    fn log(s: &str);
+}
+
+#[derive(Debug)]
+#[wasm_bindgen]
+pub struct Counter {
+    key: char,
+    count: i32
+}
+
+#[wasm_bindgen]
+impl Counter {
+    pub fn default() -> Counter {
+        log("Counter::default");
+        Self::new('a', 0)
+    }
+    pub fn new(key: char, count: i32) -> Counter {
+        log(&format!("Counter::new({}, {})", key, count));
+        Counter { key: key, count: count }
+    }
+
+    pub fn key(&self) -> char {
+        log("Counter.key()");
+        self.key
+    }
+
+    pub fn count(&self) -> i32 {
+        log("Counter.count");
+        self.count
+    }
+
+    pub fn increment(&mut self) {
+        log("Counter.increment");
+        self.count += 1;
+    }
+
+    pub fn update_key(&mut self, key: char) {
+        self.key = key;
+    }
+}
+
+#[wasm_bindgen]
+pub fn parrot(key: char) -> char {
+    log(&format!("{}", key));
+    key
+}
