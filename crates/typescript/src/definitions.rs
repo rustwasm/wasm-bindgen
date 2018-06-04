fa8961e56a1dae0ab8696c5a949dea5706cf2480
Santiago Pastorino <spastorino@gmail.com>
2018-06-04 16:44:47 -0300

    Add prototype of wasm-bindgen-typescript

---
+++ crates/typescript/src/definitions.rs
@@ -0,0 +1,81 @@
+use std::collections::HashMap;
+
+// Public API types for a TypeScript project based on
+// https://github.com/Microsoft/web-build-tools/blob/master/apps/api-extractor/src/api/api-json.schema.json
+//
+// There are some attributes that are omitted because they are not relevant to
+// us.
+#[derive(Serialize, Deserialize, Debug)]
+pub(crate) struct TsPackage {
+    kind: String,
+    name: String,
+    pub(crate) exports: HashMap<String, TsExport>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(tag = "kind")]
+pub(crate) enum TsExport {
+    #[serde(rename = "class")]
+    TsClass {
+        members: HashMap<String, TsClassMembers>,
+    },
+
+    #[serde(rename = "function")]
+    TsFunction {
+        parameters: HashMap<String, TsMethodProperty>,
+        #[serde(rename = "returnValue")]
+        return_value: TsReturnValue,
+    },
+
+    //TODO: implement ...
+    //{ "$ref": "#/definitions/interfaceApiItem" },
+    //{ "$ref": "#/definitions/namespaceApiItem" },
+    //{ "$ref": "#/definitions/enumApiItem" },
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(tag = "kind")]
+pub(crate) enum TsClassMembers {
+    #[serde(rename = "property")]
+    TsProperty {
+        #[serde(rename = "isStatic")]
+        is_static: bool,
+        #[serde(rename = "isReadOnly")]
+        is_read_only: bool,
+        #[serde(rename = "type")]
+        property_type: String,
+    },
+
+    #[serde(rename = "constructor")]
+    TsConstructor {
+        parameters: HashMap<String, TsMethodProperty>,
+    },
+
+    #[serde(rename = "method")]
+    TsMethod {
+        #[serde(rename = "accessModifier")]
+        access_modifier: String,
+        #[serde(rename = "isStatic")]
+        is_static: bool,
+        parameters: HashMap<String, TsMethodProperty>,
+        #[serde(rename = "returnValue")]
+        return_value: TsReturnValue,
+    },
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub(crate) struct TsMethodProperty {
+    name: String,
+    #[serde(rename = "type")]
+    pub(crate) property_type: String,
+    #[serde(rename = "isSpread")]
+    is_spread: bool,
+    #[serde(rename = "isOptional")]
+    is_optional: bool,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub(crate) struct TsReturnValue {
+    #[serde(rename = "type")]
+    pub(crate) property_type: String,
+}
---
+++ crates/typescript/src/main.rs
@@ -0,0 +1,16 @@
+extern crate proc_macro2;
+extern crate quote;
+extern crate wasm_bindgen_typescript;
+
+use wasm_bindgen_typescript::parser;
+
+use proc_macro2::TokenStream;
+use quote::ToTokens;
+
+fn main() {
+    let program = parser::ts_to_program("dist/wasm.api.json");
+
+    let mut tokens = TokenStream::new();
+    program.to_tokens(&mut tokens);
+    println!("{}", tokens);
+}
