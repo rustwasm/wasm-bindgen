d89c7958ebb5f37c61c417b43993efc484a57590
Jonathan Sundqvist <sundqvist.jonathan@gmail.com>
2018-06-20 20:46:04 +0200

    Js binding for to_string

--- tests/all/js_globals/Object.rs
+++ tests/all/js_globals/Object.rs
@@ -54,3 +54,36 @@ fn has_own_property() {
         "#)
         .test()
 }
+
+#[test]
+fn to_string() {
+    project()
+        .file("src/lib.rs", r#"
+            #![feature(proc_macro, wasm_custom_section)]
+
+            extern crate wasm_bindgen;
+            use wasm_bindgen::prelude::*;
+            use wasm_bindgen::js;
+
+            #[wasm_bindgen]
+            pub fn to_string(obj: &js::Object) -> String {
+                obj.to_string()
+            }
+
+            #[wasm_bindgen]
+            pub fn test() {
+                let object = js::Object::new();
+                assert_eq!(object.to_string(), "[object Object]");
+            }
+        "#)
+        .file("test.ts", r#"
+            import * as assert from "assert";
+            import * as wasm from "./out";
+
+            export function test() {
+                assert.equal(wasm.to_string({ foo: 42 }), "[object Object]");
+                wasm.test();
+            }
+        "#)
+        .test()
+}
