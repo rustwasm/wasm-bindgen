717e260a99cb2b79eb67067491d3ba2fb4f0e398
Michael Hoffmann <brennan.brisad@gmail.com>
2018-07-10 23:49:14 +0200

    Add Response interface part of fetch API

---
+++ crates/web-sys/tests/all/response.rs
@@ -0,0 +1,37 @@
+use super::project;
+
+#[test]
+fn response() {
+    project()
+        .add_local_dependency("web-sys", env!("CARGO_MANIFEST_DIR"))
+        .headless(true)
+        .file(
+            "src/lib.rs",
+            r#"
+                #![feature(proc_macro, wasm_custom_section)]
+                extern crate wasm_bindgen;
+                use wasm_bindgen::prelude::*;
+                extern crate web_sys;
+
+                #[wasm_bindgen]
+                pub fn test_response(response: &web_sys::Response) {
+                    assert!(!response.ok());
+                    assert!(!response.redirected());
+                    assert_eq!(response.status(), 501);
+                }
+            "#,
+        )
+        .file(
+            "test.js",
+            r#"
+                import * as assert from "assert";
+                import * as wasm from "./out";
+
+                export function test() {
+                    let response = new Response(null, {status: 501});
+                    wasm.test_response(response);
+                }
+            "#,
+        )
+        .test();
+}
