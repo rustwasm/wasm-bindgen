d978878d862d7c4fd6fde710c1c44f87bec196ca
Alex Crichton <alex@alexcrichton.com>
2018-08-06 09:59:05 -0700

    Remove `add_headless` test
    We've got tons of other headless tests, no need for this particular one to stick
    around!

--- tests/all/simple.rs
+++ tests/all/simple.rs
@@ -1,39 +1,6 @@
 use super::project;
 
 #[test]
-fn add_headless() {
-    project()
-        .headless(true)
-        .file(
-            "src/lib.rs",
-            r#"
-                #![feature(use_extern_macros)]
-                extern crate wasm_bindgen;
-                use wasm_bindgen::prelude::*;
-
-                #[wasm_bindgen]
-                pub fn add(a: u32, b: u32) -> u32 {
-                    a + b
-                }
-            "#,
-        )
-        .file(
-            "test.js",
-            r#"
-                import * as assert from "assert";
-                import * as wasm from "./out";
-
-                export function test() {
-                    console.log("start `add_headless` test");
-                    assert.strictEqual(wasm.add(1, 2), 3);
-                    console.log("end `add_headless` test");
-               }
-            "#,
-        )
-        .test();
-}
-
-#[test]
 fn no_std() {
     project()
         .no_std(true)
