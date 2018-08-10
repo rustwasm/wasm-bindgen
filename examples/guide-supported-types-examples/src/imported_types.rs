e87498e9391198a5c6eadd89ba10414b9c1c76a9
Nick Fitzgerald <fitzgen@gmail.com>
2018-08-10 16:56:40 -0700

    guide: Start adding example usage to "supported types" section

---
+++ examples/guide-supported-types-examples/src/imported_types.rs
@@ -0,0 +1,25 @@
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+extern {
+    pub type SomeJsType;
+}
+
+#[wasm_bindgen]
+pub fn imported_type_by_value(x: SomeJsType) { /* ... */ }
+
+#[wasm_bindgen]
+pub fn imported_type_by_shared_ref(x: &SomeJsType) { /* ... */ }
+
+#[wasm_bindgen]
+pub fn return_imported_type() -> SomeJsType {
+    unimplemented!()
+}
+
+#[wasm_bindgen]
+pub fn take_option_imported_type(x: Option<SomeJsType>) { /* ... */ }
+
+#[wasm_bindgen]
+pub fn return_option_imported_type() -> Option<SomeJsType> {
+    unimplemented!()
+}
