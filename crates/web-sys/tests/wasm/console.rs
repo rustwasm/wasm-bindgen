7840367476bb14fafa1466fa24e127682cf41ced
Anton Danilkin <afdw@yandex.ru>
2018-08-13 19:21:05 +0300

    Fix console test

--- crates/web-sys/tests/wasm/console.rs
+++ crates/web-sys/tests/wasm/console.rs
@@ -1,9 +1,8 @@
 use wasm_bindgen_test::*;
-use wasm_bindgen::prelude::*;
 use web_sys::console;
 
 #[wasm_bindgen_test]
 fn test_console() {
-    console::time("test label");
-    console::time_end("test label");
+    console::time_using_label("test label");
+    console::time_end_using_label("test label");
 }
--- crates/webidl-tests/namespace.webidl
+++ crates/webidl-tests/namespace.webidl
@@ -1,4 +1,4 @@
 namespace math_test {
-    long add_one(long val);
-    double pow(double base, double exponent);
+  long add_one(long val);
+  double pow(double base, double exponent);
 };
