14cb2dd4cf0f5e3f689ac9f9f762cffaa4a33bf3
Alex Crichton <alex@alexcrichton.com>
2018-08-30 14:26:22 -0700

    Fix fetch example ... again?

--- examples/fetch/src/lib.rs
+++ examples/fetch/src/lib.rs
@@ -46,7 +46,7 @@ pub fn run() -> Promise {
     request_options.method("GET");
     request_options.mode(RequestMode::Cors);
 
-    let req = Request::new_with_str_and_request_init("https://api.github.com/repos/rustwasm/wasm-bindgen/branches/master", &request_options).unwrap();
+    let req = Request::new_with_str_and_init("https://api.github.com/repos/rustwasm/wasm-bindgen/branches/master", &request_options).unwrap();
 
     // the RequestInit struct will eventually support setting headers, but that's missing right now
     req.headers().set("Accept", "application/vnd.github.v3+json").unwrap();
