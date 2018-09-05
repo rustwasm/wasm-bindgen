a4e258840b58564b0126b8913461f8358b9b4693
Alex Crichton <alex@alexcrichton.com>
2018-09-05 12:55:12 -0700

    Add some crate docs and such

--- crates/web-sys/src/lib.rs
+++ crates/web-sys/src/lib.rs
@@ -1,3 +1,16 @@
+//! Raw API bindings for Web APIs
+//!
+//! This is a procedurally generated crate from browser WebIDL which provides a
+//! binding to all APIs that browser provide on the web.
+//!
+//! This crate by default contains very little when compiled as almost all of
+//! its exposed APIs are gated by Cargo features. The exhaustive list of
+//! features can be found in `crates/web-sys/Cargo.toml`, but the rule of thumb
+//! for `web-sys` is that each type has its own cargo feature (named after the
+//! type). Using an API requires enabling the features for all types used in the
+//! API, and APIs should mention in the documentation what features they
+//! require.
+
 #![doc(html_root_url = "https://docs.rs/web-sys/0.2")]
 
 extern crate wasm_bindgen;
