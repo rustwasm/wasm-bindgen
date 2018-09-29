717cfa303d2340bdb865cc2d51670395959b837b
Alex Crichton <alex@alexcrichton.com>
2018-09-28 20:01:46 -0700

    Fix polyfill of `TextEncoder` and `TextDecoder`
    This commit does a few things, including:
    
    * Fixing the generated JS of `wasm-bindgen` to allow polyfills to work.
      (a minor tweak of the generated JS)
    
    * All examples are updated to include a Webpack-specific polyfill for
      these two types to get examples working in Edge.
    
    * A new page has been added to the guide about supported browsers. This
      mentions known caveats like IE 11 requiring `wasm2js` as well as
      documenting some `TextEncoder` and `TextDecoder` workarounds for Edge.
    
    Closes #895

--- examples/dom/webpack.config.js
+++ examples/dom/webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
 
 module.exports = {
     entry: './index.js',
@@ -10,6 +11,12 @@ module.exports = {
     plugins: [
         new HtmlWebpackPlugin({
             template: "index.html"
+        }),
+        // Have this example work in Edge which doesn't ship `TextEncoder` or
+        // `TextDecoder` at this time.
+        new webpack.ProvidePlugin({
+          TextDecoder: ['text-encoding', 'TextDecoder'],
+          TextEncoder: ['text-encoding', 'TextEncoder']
         })
     ],
     mode: 'development'
--- examples/julia_set/webpack.config.js
+++ examples/julia_set/webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
 
 module.exports = {
     entry: './index.js',
@@ -10,10 +11,13 @@ module.exports = {
     plugins: [
         new HtmlWebpackPlugin({
             template: "index.html"
+        }),
+        // Have this example work in Edge which doesn't ship `TextEncoder` or
+        // `TextDecoder` at this time.
+        new webpack.ProvidePlugin({
+          TextDecoder: ['text-encoding', 'TextDecoder'],
+          TextEncoder: ['text-encoding', 'TextEncoder']
         })
     ],
-    mode: 'development',
-    devServer: {
-        open: true
-    }
+    mode: 'development'
 };
