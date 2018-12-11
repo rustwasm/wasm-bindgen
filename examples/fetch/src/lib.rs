2d9dcf6ace31544a7908b8b4abf71b7fa6aa994f
T5uku5hi <misaki.makino@gmail.com>
2018-12-12 00:48:10 +0900

    run cargo fix --edition-idioms

--- examples/fetch/src/lib.rs
+++ examples/fetch/src/lib.rs
@@ -1,8 +1,8 @@
-extern crate futures;
-extern crate js_sys;
-extern crate wasm_bindgen;
-extern crate wasm_bindgen_futures;
-extern crate web_sys;
+
+
+
+
+use web_sys;
 #[macro_use]
 extern crate serde_derive;
 
