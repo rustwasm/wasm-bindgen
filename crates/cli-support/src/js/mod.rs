6e2f10440e7bb25c269aa2bf95f065b129d66e64
rhysd <lin90162@yahoo.co.jp>
2019-02-01 16:25:25 +0900

    give 2 space indentations to each enum variant

--- crates/cli-support/src/js/mod.rs
+++ crates/cli-support/src/js/mod.rs
@@ -2573,12 +2573,12 @@ impl<'a, 'b> SubContext<'a, 'b> {
             .typescript
             .push_str(&format!("export enum {} {{", enum_.name));
 
-        variants.clear();
         for variant in enum_.variants.iter() {
-            variants.push_str(&format!("{},", variant.name));
+            self.cx
+                .typescript
+                .push_str(&format!("\n  {},", variant.name));
         }
-        self.cx.typescript.push_str(&variants);
-        self.cx.typescript.push_str("}\n");
+        self.cx.typescript.push_str("\n}\n");
     }
 
     fn generate_struct(&mut self, struct_: &decode::Struct) -> Result<(), Error> {
