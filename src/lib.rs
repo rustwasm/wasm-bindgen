d9e53ac2f6fdc9b659e1b6a812f860c1cf408eab
Alex Crichton <alex@alexcrichton.com>
2019-06-25 04:55:16 -0700

    Switch to `debug_assert_eq!` in intrinic
    Reduce the code size of wasm files by avoiding an `assert_eq!` and panic
    code in production builds.

--- src/lib.rs
+++ src/lib.rs
@@ -1072,7 +1072,7 @@ pub mod __rt {
 
     #[no_mangle]
     pub unsafe extern "C" fn __wbindgen_exn_store(idx: u32) {
-        assert_eq!(GLOBAL_EXNDATA[0], 0);
+        debug_assert_eq!(GLOBAL_EXNDATA[0], 0);
         GLOBAL_EXNDATA[0] = 1;
         GLOBAL_EXNDATA[1] = idx;
     }
