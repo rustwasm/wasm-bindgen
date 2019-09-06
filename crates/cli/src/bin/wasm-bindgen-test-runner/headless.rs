9c01e39b5a3ed87e8e6b6eeb75e382e7c04643a9
Christopher Burgos <cburgos294@gmail.com>
2019-09-06 19:59:22 -0400

    remove shell status update

--- crates/cli/src/bin/wasm-bindgen-test-runner/headless.rs
+++ crates/cli/src/bin/wasm-bindgen-test-runner/headless.rs
@@ -34,7 +34,6 @@ pub fn run(server: &SocketAddr, shell: &Shell) -> Result<(), Error> {
 
     // Spawn the driver binary, collecting its stdout/stderr in separate
     // threads. We'll print this output later.
-    shell.status("Spawning Geckodriver...");
     let mut cmd = Command::new(driver.path());
     cmd.args(&args)
         .arg(format!("--port={}", driver_addr.port().to_string()));
