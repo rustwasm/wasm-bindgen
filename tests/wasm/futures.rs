9c330529f2277e2b34a8189b60c58438b77f3407
Alex Crichton <alex@alexcrichton.com>
2019-09-20 16:06:27 -0500

    Fix codegen for descriptors of async fn returns (#1782)
    They erroneously reported returning the original return type, not the
    promise! Let's also add a bunch of positive tests while we're at it.
    
    Closes #1781

---
+++ tests/wasm/futures.rs
@@ -0,0 +1,70 @@
+use wasm_bindgen::prelude::*;
+use wasm_bindgen_test::*;
+
+#[wasm_bindgen(module = "tests/wasm/futures.js")]
+extern "C" {
+    fn call_exports() -> js_sys::Promise;
+}
+
+#[wasm_bindgen_test]
+async fn smoke() {
+    wasm_bindgen_futures::JsFuture::from(call_exports()).await.unwrap();
+}
+
+#[wasm_bindgen]
+pub async fn async_do_nothing() {}
+
+#[wasm_bindgen]
+pub async fn async_return_1() -> JsValue {
+    1.into()
+}
+
+#[wasm_bindgen]
+pub async fn async_return_2() -> u32 {
+    2
+}
+
+#[wasm_bindgen]
+pub async fn async_nothing_again() -> Result<(), JsValue> {
+    Ok(())
+}
+
+#[wasm_bindgen]
+pub async fn async_return_3() -> Result<u32, JsValue> {
+    Ok(3)
+}
+
+#[wasm_bindgen]
+pub async fn async_return_4() -> Result<JsValue, JsValue> {
+    Ok(4.into())
+}
+
+#[wasm_bindgen]
+pub struct AsyncCustomReturn {
+    pub val: u32,
+}
+
+#[wasm_bindgen]
+pub async fn async_return_5() -> AsyncCustomReturn {
+    AsyncCustomReturn { val: 5 }
+}
+
+#[wasm_bindgen]
+pub async fn async_return_6() -> Result<AsyncCustomReturn, JsValue> {
+    Ok(AsyncCustomReturn { val: 6 })
+}
+
+#[wasm_bindgen]
+pub async fn async_return_7() -> Result<AsyncCustomReturn, u32> {
+    Ok(AsyncCustomReturn { val: 7 })
+}
+
+#[wasm_bindgen]
+pub async fn async_return_8() -> Result<AsyncCustomReturn, AsyncCustomReturn> {
+    Ok(AsyncCustomReturn { val: 8 })
+}
+
+#[wasm_bindgen]
+pub async fn async_throw() -> Result<(), js_sys::Error> {
+    Err(js_sys::Error::new("async message"))
+}
