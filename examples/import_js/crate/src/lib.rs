aab99feb3e1de666069c6157469b127bda193dd6
Fernando Bitti Loureiro <fernando.bitti@gmail.com>
2020-01-06 18:47:13 +0100

    The example should output "Hello from Rust!" (#1931)
    * Make console output "Hello from Rust!"
    
    The HTML says the console would output Hello from Rust!, but instead it outputs Hello, World!
    This is a proposed fix.
    
    * Output "Hello from Rust!"
    
    The HTML says the console would output "Hello from Rust!" but instead it outputs "Hello, World!".
    This is a proposed fix.

--- examples/import_js/crate/src/lib.rs
+++ examples/import_js/crate/src/lib.rs
@@ -26,7 +26,7 @@ extern "C" {
 
 #[wasm_bindgen(start)]
 pub fn run() {
-    log(&format!("Hello, {}!", name()));
+    log(&format!("Hello from {}!", name())); // should output "Hello from Rust!"
 
     let x = MyClass::new();
     assert_eq!(x.number(), 42);
