34eb8a85166c089e2b1f4f2400c24fc6d1e38871
Mario Reder <mreder1289@gmail.com>
2020-01-21 20:04:40 +0100

    fix: ignore non dependency keys in package json (#1969)
    resolves #1921

--- crates/cli/tests/wasm-bindgen/npm.rs
+++ crates/cli/tests/wasm-bindgen/npm.rs
@@ -35,8 +35,8 @@ compatible with the `bundler` and `nodejs` targets
 }
 
 #[test]
-fn more_package_json_fields_rejected() {
-    let (mut cmd, _out_dir) = Project::new("more_package_json_fields_rejected")
+fn more_package_json_fields_ignored() {
+    let (mut cmd, _out_dir) = Project::new("more_package_json_fields_ignored")
         .file(
             "src/lib.rs",
             r#"
@@ -63,16 +63,7 @@ fn more_package_json_fields_rejected() {
             "#,
         )
         .wasm_bindgen("");
-    cmd.assert()
-        .stderr(
-            str::is_match(
-                "\
-error: NPM manifest found at `.*` can currently only have one key, .*
-",
-            )
-            .unwrap(),
-        )
-        .failure();
+    cmd.assert().success();
 }
 
 #[test]
