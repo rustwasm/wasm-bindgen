6cd5adace433131c736c9eb6bac347ff74f1f0ff
Pauan <pauanyu+github@pm.me>
2020-03-04 03:38:05 +0100

    Improving the CHANGELOG and docs

--- examples/guide-supported-types-examples/src/imported_types.rs
+++ examples/guide-supported-types-examples/src/imported_types.rs
@@ -1,6 +1,26 @@
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
+pub enum NumberEnum {
+    Foo = 0,
+    Bar = 1,
+    Qux = 2,
+}
+
+#[wasm_bindgen]
+pub enum StringEnum {
+    Foo = "foo",
+    Bar = "bar",
+    Qux = "qux",
+}
+
+#[wasm_bindgen]
+pub struct Struct {
+    pub number: NumberEnum,
+    pub string: StringEnum,
+}
+
+#[wasm_bindgen]
 extern "C" {
     pub type SomeJsType;
 }
