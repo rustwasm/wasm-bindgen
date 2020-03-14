95f3e57b0fb5fb4beade4ea10b3af22b86eacc7b
Kev Kirkland <kev.kirkland@elucidata.co.uk>
2020-03-14 19:27:45 +0000

    Tidy up WebXR example code.

--- examples/webxr/src/lib.rs
+++ examples/webxr/src/lib.rs
@@ -7,7 +7,6 @@ use wasm_bindgen::prelude::*;
 use wasm_bindgen::JsCast;
 use wasm_bindgen_futures::future_to_promise;
 use wasm_bindgen_futures::JsFuture;
-// use web_sys::{HtmlCanvasElement, WebGl2RenderingContext, XrFrame, XrRenderStateInit, XrSession, XrSessionMode, XrWebGlLayer};
 use web_sys::*;
 use std::cell::RefCell;
 use std::collections::HashMap;
@@ -21,53 +20,6 @@ macro_rules! log {
         web_sys::console::log_1(&format!( $( $t )* ).into());
     }
 }
-
-// #[wasm_bindgen]
-// pub async fn run_webxr() {
-//     log!("Starting WebXR...");
-//     let navigator: web_sys::Navigator = web_sys::window().unwrap().navigator();
-//     let xr = navigator.xr();
-//     let session_mode = XrSessionMode::Inline;
-//     let session_supported_promise = xr.is_session_supported(session_mode);
-//     let supports_session = wasm_bindgen_futures::JsFuture::from(session_supported_promise).await;
-//     let supports_session = supports_session.unwrap();
-//     if supports_session == false {
-//         log!("XR session not supported");
-//         return()
-//     }
-//     let session_promise = xr.request_session(session_mode);
-//     let session = wasm_bindgen_futures::JsFuture::from(session_promise).await;
-//     let session: XrSession = session.unwrap().into();
-
-//     // Reuse the logic from the requestAnimationFrame() example
-//     let f = Rc::new(RefCell::new(None));
-//     let g = f.clone();
-
-//     let mut i = 0;
-//     *g.borrow_mut() = Some(Closure::wrap(Box::new(move |time: f64, frame: XrFrame| {
-//         if i > 300 {
-//             log!("All done!");
-
-//             // Drop our handle to this closure so that it will get cleaned
-//             // up once we return.
-//             let _ = f.borrow_mut().take();
-//             return;
-//         }
-
-//         i += 1;
-//         let text = format!("requestAnimationFrame has been called {} times.", i);
-//         log!("{}", text);
-
-//         // Schedule ourself for another requestAnimationFrame callback.
-//         let inner_session: XrSession = frame.session();
-//         request_animation_frame(&inner_session, f.borrow().as_ref().unwrap());
-//     }) as Box<dyn FnMut(f64, XrFrame)>));
-
-//     log!("Starting animation frame...");
-//     request_animation_frame(&session, g.borrow().as_ref().unwrap());
-
-//     ()
-// }
 
 
 fn request_animation_frame(session: &XrSession, f: &Closure<dyn FnMut(f64, XrFrame)>) -> i32 {
@@ -145,7 +97,6 @@ impl XrApp {
             let supports_session = supports_session.unwrap();
             if supports_session == false {
                 log!("XR session not supported");
-                // return future_to_promise(async { Ok(JsValue::from("XR session not supported"))});
                 return Ok(JsValue::from("XR session not supported"));
             }
             
@@ -173,6 +124,7 @@ impl XrApp {
 
         let mut i = 0;
         *g.borrow_mut() = Some(Closure::wrap(Box::new(move |time: f64, frame: XrFrame| {
+            log!("Frame rendering...");
             if i > 2 {
                 log!("All done!");
 
@@ -186,89 +138,14 @@ impl XrApp {
             i += 1;
 
             // Schedule ourself for another requestAnimationFrame callback.
-            // TODO: WebXR samples call this at top of request_animation_frame - should this be moved?
-            // request_animation_frame(&sess, f.borrow().as_ref().unwrap());
+            // TODO: WebXR Samples call this at top of request_animation_frame - should this be moved?
+            request_animation_frame(&sess, f.borrow().as_ref().unwrap());
             
         }) as Box<dyn FnMut(f64, XrFrame)>));
 
-        // let session_refcell: &RefCell<Option<XrSession>> = self.session.as_ref();
         let session: &Option<XrSession> = &self.session.borrow();
-        // let sess: &Option<XrSession> = session_refcell.borrow();
-
-        // let sess = if let Some(val) = session { sess } else { return () };
         let sess: &XrSession = if let Some(sess) = session { sess } else { return () };
 
-        // sess.request_animation_frame(g.borrow().as_ref().unwrap());
-        // sess.request_animation_frame(g.borrow().as_ref().unwrap().as_ref().unchecked_ref());
-
-        // match session {
-        //     Some(sess) => {
-        //         log!("There is a session");
-        //         // let i: i32 = request_animation_frame(&sess, g.borrow().as_ref().unwrap());
-        //         let h = g.borrow().as_ref().unwrap();
-        //         let i: i32 = sess.request_animation_frame(h.as_ref().unchecked_ref());
-        //     },
-        //     None => {}
-        // }
-    }
-
-    pub fn start_rendering(&self) {
-        // let session: &XrSession;
-
-        // if self.session.borrow().as_ref() == None {
-        //     return ();
-        // }
-        // else {
-        //     session = self.session.borrow().as_ref().unwrap();
-        // }
-
-        // match self.session.borrow().as_ref() {
-        //     Some(s) => session = s,
-        //     None => { log!("No XR session. Aborting"); return ();}
-        // }
-
-        // match self.session.borrow().borrow() {
-        //     Some(s) => session = s,
-        //     None => { log!("No XR session. Aborting"); return ();}
-        // }
-
-        
-
-        log!("Starting animation frame...");
-        // request_animation_frame(&session, g.borrow().as_ref().unwrap());
-
-        match &self.session.borrow().as_ref() {
-            Some(s) => {
-                // Reuse the logic from the requestAnimationFrame() example
-                let f = Rc::new(RefCell::new(None));
-                let g = f.clone();
-
-                let mut i = 0;
-                *g.borrow_mut() = Some(Closure::wrap(Box::new(move |time: f64, frame: XrFrame| {
-                    if i > 2 {
-                        log!("All done!");
-
-                        // Drop our handle to this closure so that it will get cleaned
-                        // up once we return.
-                        let _ = f.borrow_mut().take();
-                        return;
-                    }
-
-                    i += 1;
-                    let text = format!("requestAnimationFrame has been called {} times. Time {}", i, time);
-                    log!("{}", text);
-
-                    // Schedule ourself for another requestAnimationFrame callback.
-                    // let inner_session: XrSession = frame.session();
-                    // request_animation_frame(&inner_session, f.borrow().as_ref().unwrap());
-                }) as Box<dyn FnMut(f64, XrFrame)>));
-
-                request_animation_frame(s, g.borrow().as_ref().unwrap());
-            },
-            // Some(s) => { log!("Has XR session. Rendering"); return (); }
-            None => {
-                log!("No XR session. Aborting"); return ();
-            }
-        }
+        request_animation_frame(sess, g.borrow().as_ref().unwrap());
     }
 }
