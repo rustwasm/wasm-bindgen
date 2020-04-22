e7361d8a36134404d7850075e496e9f0a5c3080a
Vojtěch Pejša <vojtechpejsa7@gmail.com>
2020-04-22 14:21:18 +0200

    Generate Clipboard APIs

--- crates/web-sys/src/features/gen_ClipboardEventInit.rs
+++ crates/web-sys/src/features/gen_ClipboardEventInit.rs
@@ -1,6 +1,7 @@
 #![allow(unused_imports)]
 use super::*;
 use wasm_bindgen::prelude::*;
+#[cfg(web_sys_unstable_apis)]
 #[wasm_bindgen]
 extern "C" {
     # [ wasm_bindgen ( extends = :: js_sys :: Object , js_name = ClipboardEventInit ) ]
@@ -8,20 +9,31 @@ extern "C" {
     #[doc = "The `ClipboardEventInit` dictionary."]
     #[doc = ""]
     #[doc = "*This API requires the following crate features to be activated: `ClipboardEventInit`*"]
+    #[doc = ""]
+    #[doc = "*This API is unstable and requires `--cfg=web_sys_unstable_apis` to be activated, as"]
+    #[doc = "[described in the `wasm-bindgen` guide](https://rustwasm.github.io/docs/wasm-bindgen/web-sys/unstable-apis.html)*"]
     pub type ClipboardEventInit;
 }
+#[cfg(web_sys_unstable_apis)]
 impl ClipboardEventInit {
     #[doc = "Construct a new `ClipboardEventInit`."]
     #[doc = ""]
     #[doc = "*This API requires the following crate features to be activated: `ClipboardEventInit`*"]
+    #[doc = ""]
+    #[doc = "*This API is unstable and requires `--cfg=web_sys_unstable_apis` to be activated, as"]
+    #[doc = "[described in the `wasm-bindgen` guide](https://rustwasm.github.io/docs/wasm-bindgen/web-sys/unstable-apis.html)*"]
     pub fn new() -> Self {
         #[allow(unused_mut)]
         let mut ret: Self = ::wasm_bindgen::JsCast::unchecked_into(::js_sys::Object::new());
         ret
     }
+    #[cfg(web_sys_unstable_apis)]
     #[doc = "Change the `bubbles` field of this object."]
     #[doc = ""]
     #[doc = "*This API requires the following crate features to be activated: `ClipboardEventInit`*"]
+    #[doc = ""]
+    #[doc = "*This API is unstable and requires `--cfg=web_sys_unstable_apis` to be activated, as"]
+    #[doc = "[described in the `wasm-bindgen` guide](https://rustwasm.github.io/docs/wasm-bindgen/web-sys/unstable-apis.html)*"]
     pub fn bubbles(&mut self, val: bool) -> &mut Self {
         use wasm_bindgen::JsValue;
         let r = ::js_sys::Reflect::set(
@@ -36,9 +48,13 @@ impl ClipboardEventInit {
         let _ = r;
         self
     }
+    #[cfg(web_sys_unstable_apis)]
     #[doc = "Change the `cancelable` field of this object."]
     #[doc = ""]
     #[doc = "*This API requires the following crate features to be activated: `ClipboardEventInit`*"]
+    #[doc = ""]
+    #[doc = "*This API is unstable and requires `--cfg=web_sys_unstable_apis` to be activated, as"]
+    #[doc = "[described in the `wasm-bindgen` guide](https://rustwasm.github.io/docs/wasm-bindgen/web-sys/unstable-apis.html)*"]
     pub fn cancelable(&mut self, val: bool) -> &mut Self {
         use wasm_bindgen::JsValue;
         let r = ::js_sys::Reflect::set(
@@ -53,9 +69,13 @@ impl ClipboardEventInit {
         let _ = r;
         self
     }
+    #[cfg(web_sys_unstable_apis)]
     #[doc = "Change the `composed` field of this object."]
     #[doc = ""]
     #[doc = "*This API requires the following crate features to be activated: `ClipboardEventInit`*"]
+    #[doc = ""]
+    #[doc = "*This API is unstable and requires `--cfg=web_sys_unstable_apis` to be activated, as"]
+    #[doc = "[described in the `wasm-bindgen` guide](https://rustwasm.github.io/docs/wasm-bindgen/web-sys/unstable-apis.html)*"]
     pub fn composed(&mut self, val: bool) -> &mut Self {
         use wasm_bindgen::JsValue;
         let r = ::js_sys::Reflect::set(
@@ -70,27 +90,19 @@ impl ClipboardEventInit {
         let _ = r;
         self
     }
-    #[doc = "Change the `data` field of this object."]
+    #[cfg(web_sys_unstable_apis)]
+    #[cfg(feature = "DataTransfer")]
+    #[doc = "Change the `clipboardData` field of this object."]
     #[doc = ""]
-    #[doc = "*This API requires the following crate features to be activated: `ClipboardEventInit`*"]
-    pub fn data(&mut self, val: &str) -> &mut Self {
-        use wasm_bindgen::JsValue;
-        let r = ::js_sys::Reflect::set(self.as_ref(), &JsValue::from("data"), &JsValue::from(val));
-        debug_assert!(
-            r.is_ok(),
-            "setting properties should never fail on our dictionary objects"
-        );
-        let _ = r;
-        self
-    }
-    #[doc = "Change the `dataType` field of this object."]
+    #[doc = "*This API requires the following crate features to be activated: `ClipboardEventInit`, `DataTransfer`*"]
     #[doc = ""]
-    #[doc = "*This API requires the following crate features to be activated: `ClipboardEventInit`*"]
-    pub fn data_type(&mut self, val: &str) -> &mut Self {
+    #[doc = "*This API is unstable and requires `--cfg=web_sys_unstable_apis` to be activated, as"]
+    #[doc = "[described in the `wasm-bindgen` guide](https://rustwasm.github.io/docs/wasm-bindgen/web-sys/unstable-apis.html)*"]
+    pub fn clipboard_data(&mut self, val: Option<&DataTransfer>) -> &mut Self {
         use wasm_bindgen::JsValue;
         let r = ::js_sys::Reflect::set(
             self.as_ref(),
-            &JsValue::from("dataType"),
+            &JsValue::from("clipboardData"),
             &JsValue::from(val),
         );
         debug_assert!(
