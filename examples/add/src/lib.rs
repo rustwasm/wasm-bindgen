3725e7157d12bcf50b806929132ce0bf25aca1f4
Alex Crichton <alex@alexcrichton.com>
2020-06-05 07:24:46 -0700

    Remove accidental debugging code added to example

--- examples/add/src/lib.rs
+++ examples/add/src/lib.rs
@@ -4,21 +4,3 @@ use wasm_bindgen::prelude::*;
 pub fn add(a: u32, b: u32) -> u32 {
     a + b
 }
-
-#[wasm_bindgen]
-#[derive(Copy, Clone)]
-pub struct Answer(u32);
-
-#[wasm_bindgen]
-impl Answer {
-    pub fn new() -> Answer {
-        Answer(41)
-    }
-    #[wasm_bindgen(getter)]
-    pub fn the_answer(self) -> u32 {
-        self.0 + 1
-    }
-    pub fn foo(self) -> u32 {
-        self.0 + 1
-    }
-}
