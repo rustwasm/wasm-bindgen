97997699427151ae023a13b79bff601f06bfcb88
Ash Wolf <ninji@wuffs.org>
2020-07-16 16:00:30 +0100

    update test to check optional enum as well

--- crates/typescript-tests/src/enums.rs
+++ crates/typescript-tests/src/enums.rs
@@ -9,6 +9,15 @@ pub enum Shape {
 pub fn accepts_enum(_: Shape) {}
 
 #[wasm_bindgen]
-pub fn take_and_return_shape(s: Shape) -> Shape {
+pub fn take_and_return_enum(s: Shape) -> Shape {
+    s
+}
+
+
+#[wasm_bindgen]
+pub fn accepts_option_enum(_: Option<Shape>) {}
+
+#[wasm_bindgen]
+pub fn take_and_return_option_enum(s: Option<Shape>) -> Option<Shape> {
     s
 }
--- crates/typescript-tests/src/enums.ts
+++ crates/typescript-tests/src/enums.ts
@@ -1,4 +1,7 @@
 import * as wbg from '../pkg/typescript_tests';
 
 const wbg_accepts_enum: (a: wbg.Shape) => void = wbg.accepts_enum;
-const take_and_return_shape: (a: wbg.Shape) => wbg.Shape = wbg.take_and_return_shape;
+const wbg_take_and_return_enum: (a: wbg.Shape) => wbg.Shape = wbg.take_and_return_enum;
+
+const wbg_accepts_option_enum: (a: wbg.Shape | undefined) => void = wbg.accepts_option_enum;
+const wbg_take_and_return_option_enum: (a: wbg.Shape | undefined) => wbg.Shape | undefined = wbg.take_and_return_option_enum;
