0be6887089d19f11bda7a821410e40ae941e7539
Alex Crichton <alex@alexcrichton.com>
2020-08-29 11:53:14 -0700

    Update a reference test for 1.46

--- crates/cli/tests/reference/import-catch.js
+++ crates/cli/tests/reference/import-catch.js
@@ -45,10 +45,6 @@ export function exported() {
     wasm.exported();
 }
 
-export const __wbindgen_object_drop_ref = function(arg0) {
-    takeObject(arg0);
-};
-
 export const __wbg_foo_8d66ddef0ff279d6 = handleError(function() {
     foo();
 });
