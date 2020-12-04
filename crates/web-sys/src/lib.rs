395256169eb6c827e8236fdc4f9aa441c8180753
chrysn <chrysn@fsfe.org>
2020-12-04 11:12:10 +0100

    web-sys: Point to WebIDLs
    because generated docs will occasionally ask the user to look things up.

--- crates/web-sys/src/lib.rs
+++ crates/web-sys/src/lib.rs
@@ -1,7 +1,11 @@
 //! Raw API bindings for Web APIs
 //!
 //! This is a procedurally generated crate from browser WebIDL which provides a
-//! binding to all APIs that browsers provide on the web.
+//! binding to all APIs that browsers provide on the web. The source files are maintained [in the
+//! crate's webidl directory][1]. The generated API documentation captures most of the information
+//! contained in there – if not, it refers back there.
+//!
+//! [1]: https://github.com/rustwasm/wasm-bindgen/tree/master/crates/web-sys/webidls
 //!
 //! This crate by default contains very little when compiled as almost all of
 //! its exposed APIs are gated by Cargo features. The exhaustive list of
