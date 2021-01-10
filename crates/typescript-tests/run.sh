2b0e83ec8ee5cf68d283b22ca598cb6b5eebbf69
aaron-human <59424745+aaron-human@users.noreply.github.com>
2021-01-09 20:03:55 -0700

    Tried to get the no-modules TypeScript tests to build against actual TypeScript.
    So now the `*.d.ts` files will actually be build against (modified) `*.ts` files. This should be a better check than only verifying TypeScript doesn't fail when given just the `*.d.ts` files (what it did before).
    
    Note that the `*.ts` files are generated from the `*.ts` files in `src` using some fairly simple Regular Expressions.
    
    This skips checking the `typescript_tests_bg.wasm.d.ts` file, as I have no experience using that.

--- crates/typescript-tests/run.sh
+++ crates/typescript-tests/run.sh
@@ -32,4 +32,24 @@ cargo run -p wasm-bindgen-cli --bin wasm-bindgen -- \
   --target no-modules \
   --typescript
 
+rm -rf src_no_modules
+mkdir src_no_modules
+cd src
+for filename in *.ts; do
+  # Create TypeScript file copies that don't use the module system.
+  if grep -q "as wasm" "$filename"
+  then
+    echo "Ignoring $filename as it uses the *.wasm.d.ts file."
+  else
+    path="../src_no_modules/$filename"
+    # Copy over everything but the import statements.
+    grep -v -w "import" "$filename" > "$path"
+    # Then replace "wbg."/"wbg "/"wbg[" with "wasm_bindgen" (the namespace the .d.ts file uses).
+    sed -i "s/wbg\./wasm_bindgen./g" "$path"
+    sed -i "s/wbg /wasm_bindgen /g" "$path"
+    sed -i "s/wbg\[/wasm_bindgen[/g" "$path"
+  fi
+done
+cd ..
+
 npm run tsc -- -p no_modules.tsconfig.json
