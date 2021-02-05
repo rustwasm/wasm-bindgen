564a517d3ffe06283261aba2b172dbb250c93464
aaron-human <59424745+aaron-human@users.noreply.github.com>
2021-02-04 19:33:56 -0700

    Adding more info about the testing changes in run.sh.

--- crates/typescript-tests/run.sh
+++ crates/typescript-tests/run.sh
@@ -24,7 +24,7 @@ fi
 
 npm run tsc
 
-# Build using no-modules, and make sure can minimally be read by the TypeScript compiler.
+# Build using no-modules.
 mkdir pkg/no_modules
 cargo run -p wasm-bindgen-cli --bin wasm-bindgen -- \
   ../../target/wasm32-unknown-unknown/debug/typescript_tests.wasm \
@@ -32,19 +32,22 @@ cargo run -p wasm-bindgen-cli --bin wasm-bindgen -- \
   --target no-modules \
   --typescript
 
+# Then create copies of the TypeScript tests used on "--target web" (i.e. those in ./src) but adjust them to work with "--target no-modules".
+# Store the new generated test files under "src_no_modules".
 rm -rf src_no_modules
 mkdir src_no_modules
 cd src
 for filename in *.ts; do
-  # Create TypeScript file copies that don't use the module system.
-  if grep -q "as wasm" "$filename"
+  if grep -q "_bg.wasm" "$filename"
   then
+    # I am unsure how to import or test the "*_bg.wasm" file.
+    # So any test file that includes those is currently being skipped.
     echo "Ignoring $filename as it uses the *.wasm.d.ts file."
   else
     path="../src_no_modules/$filename"
-    # Copy over everything but the import statements.
+    # Copy over every line EXCEPT for the import statements (since "no-modules" has no modules to import).
     grep -v -w "import" "$filename" > "$path"
-    # Then replace "wbg."/"wbg "/"wbg[" with "wasm_bindgen" (the namespace the .d.ts file uses).
+    # Then replace all of the instances of "wbg" (the namespace all the tests use to import the *.d.ts files from "--target web") with "wasm_bindgen" (the namespace the `no-modules` *.d.ts files use).
     sed -i "s/wbg\./wasm_bindgen./g" "$path"
     sed -i "s/wbg /wasm_bindgen /g" "$path"
     sed -i "s/wbg\[/wasm_bindgen[/g" "$path"
@@ -52,4 +55,5 @@ for filename in *.ts; do
 done
 cd ..
 
+# Then try to build the typescript in the src_no_modules folder against the pkg/no_modules build.
 npm run tsc -- -p no_modules.tsconfig.json
