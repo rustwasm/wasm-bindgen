0a27f89d799b8635e63b93085b5a9c7f19f8476a
Teoh Han Hui <teohhanhui@gmail.com>
2021-12-25 18:08:28 +0800

    Fix macro hygiene in wasm_bindgen_test
    Specifically, use fully qualified path for `concat!` macro.

--- crates/test-macro/src/lib.rs
+++ crates/test-macro/src/lib.rs
@@ -62,7 +62,7 @@ pub fn wasm_bindgen_test(
         (quote! {
             #[no_mangle]
             pub extern "C" fn #name(cx: &::wasm_bindgen_test::__rt::Context) {
-                let test_name = concat!(module_path!(), "::", stringify!(#ident));
+                let test_name = ::std::concat!(::std::module_path!(), "::", ::std::stringify!(#ident));
                 #test_body
             }
         })
