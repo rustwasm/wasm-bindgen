3f941ff5d4a32183892997c38754d6684dfb16e1
Bill Hannah <bill.hannah@pushpay.com>
2022-01-07 15:03:50 +1300

    Remove unnecessary async import in hello world example
    In Webpack 5, wasm-based imports are now supported, and can be imported directly

--- examples/hello_world/index.js
+++ examples/hello_world/index.js
@@ -1,8 +1,3 @@
-// Note that a dynamic `import` statement here is required due to
-// webpack/webpack#6615, but in theory `import { greet } from './pkg';`
-// will work here one day as well!
-const rust = import('./pkg');
+import { greet } from './pkg';
 
-rust
-  .then(m => m.greet('World!'))
-  .catch(console.error);
+greet('World');
