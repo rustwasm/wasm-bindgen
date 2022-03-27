7ad9a7e62e971d564f29c7b9ce391281c82568e1
Sixshaman <Shaman1234@gmail.com>
2022-03-27 19:01:38 +0300

    Updates for constructors in Media Stream inserting API

--- crates/web-sys/src/features/gen_VideoTrackGenerator.rs
+++ crates/web-sys/src/features/gen_VideoTrackGenerator.rs
@@ -61,4 +61,15 @@ extern "C" {
     #[doc = "*This API is unstable and requires `--cfg=web_sys_unstable_apis` to be activated, as"]
     #[doc = "[described in the `wasm-bindgen` guide](https://rustwasm.github.io/docs/wasm-bindgen/web-sys/unstable-apis.html)*"]
     pub fn track(this: &VideoTrackGenerator) -> MediaStreamTrack;
+    #[cfg(web_sys_unstable_apis)]
+    #[wasm_bindgen(catch, constructor, js_class = "VideoTrackGenerator")]
+    #[doc = "The `new VideoTrackGenerator(..)` constructor, creating a new instance of `VideoTrackGenerator`."]
+    #[doc = ""]
+    #[doc = "[MDN Documentation](https://developer.mozilla.org/en-US/docs/Web/API/VideoTrackGenerator/VideoTrackGenerator)"]
+    #[doc = ""]
+    #[doc = "*This API requires the following crate features to be activated: `VideoTrackGenerator`*"]
+    #[doc = ""]
+    #[doc = "*This API is unstable and requires `--cfg=web_sys_unstable_apis` to be activated, as"]
+    #[doc = "[described in the `wasm-bindgen` guide](https://rustwasm.github.io/docs/wasm-bindgen/web-sys/unstable-apis.html)*"]
+    pub fn new() -> Result<VideoTrackGenerator, JsValue>;
 }
