d4cdcff2560119ba97dae3dfaa8717ca55fb6a45
Ted Driggs <ted.driggs@crowdstrike.com>
2022-04-29 12:42:58 -0700

    Remove From impls only used once

--- crates/macro-support/src/rhs.rs
+++ crates/macro-support/src/rhs.rs
@@ -14,37 +14,19 @@ pub enum Rhs {
     Expr(Expr),
 }
 
-impl From<Path> for Rhs {
-    fn from(v: Path) -> Self {
-        Self::Path(v)
-    }
-}
-
-impl From<LitStr> for Rhs {
-    fn from(v: LitStr) -> Self {
-        Self::LitStr(v)
-    }
-}
-
-impl From<Expr> for Rhs {
-    fn from(v: Expr) -> Self {
-        Self::Expr(v)
-    }
-}
-
 impl Parse for Rhs {
     fn parse(input: ParseStream) -> syn::Result<Self> {
         if input.fork().parse::<LitStr>().is_ok() {
-            input.parse::<LitStr>().map(Self::from)
+            input.parse().map(Self::LitStr)
         } else if input.fork().parse::<Path>().is_ok() {
-            input.parse::<Path>().map(Self::from)
+            input.parse().map(Self::Path)
         } else if input.fork().call(Ident::parse_any).is_ok() {
             input
                 .call(Ident::parse_any)
                 .map(syn::Path::from)
-                .map(Self::from)
+                .map(Self::Path)
         } else {
-            input.parse::<Expr>().map(Self::from)
+            input.parse().map(Self::Expr)
         }
     }
 }
