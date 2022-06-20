rustwasm/wasm-bindgen

commit 96eca588e6a647e33bdd3944f0502bb324369ffc

fix(typescript) Getters correctly define the inner results. (#2909)
* fix(typescript) Getters correctly define the inner results.

This patch fixes
https://github.com/rustwasm/wasm-bindgen/issues/2721. Let's consider
the following example:

```rust
pub struct Foo {
    /// Hello `first`.
    pub first: u32,

    /// Hello `second`.
    #[wasm_bindgen(readonly)]
    pub second: u32,
}
```

It outputs the following `.d.ts` file:

```typescript
export class Foo {
  free(): void;
/**
* Hello `first`.
*/
  first: number;
/**
* Hello `second`.
*/
  readonly second: void;
}
```

What's wrong here is that `second` has the type `void`. It should be
`number`.

What's happening? For `Foo.first`, a getter and a setter are
generated. The getter never has a return type (spoiler: that's the
bug), but the setter has a correct return type somehow (it's infererd
from its arguments, combined with a unit descriptor, see
`wasm_bindgen_cli_support::wit::Context::struct_`). When the getter
and the setter are processed, they both end up calling
`wasm_bindgen_cli_support::js::ExportedClass::push_accessor_ts`. This
function overwrites the return type for the same field everytime it is
called. So when called for the getter, the return type is missing, and
when called for the setter with a type, the bug is fixed.

For `Foo.second`, it's different. There is no setter generated because
the field is marked as `readonly`. So the bug appears.

To fix that, I've updated the getter `Function` descriptor, so that it
has an `inner_ret` value. This is passed to an `Adapter.inner_results`
at some point in the code, which is finally passed by
`JsFunction::process` to `JsFunction::typescript_signature` to
generate the typescript signature. And suddently, it's fixed!

However, we got:

```typescript
export class Foo {
  free(): void;
/**
* Hello `first`.
* @returns {number}
*/
  first: number;
/**
* Hello `second`.
* @returns {number}
*/

crates/cli-support/src/js/mod.rs
@@ -2521,9 +2521,10 @@ impl<'a> Context<'a> {
                     false => None,
                 };
 
-                let docs = format_doc_comments(&export.comments, Some(js_doc));
                 match &export.kind {
                     AuxExportKind::Function(name) => {
+                        let docs = format_doc_comments(&export.comments, Some(js_doc));
+
                         if let Some(ts_sig) = ts_sig {
                             self.typescript.push_str(&docs);
                             self.typescript.push_str("export function ");
@@ -2535,6 +2536,7 @@ impl<'a> Context<'a> {
                         self.globals.push_str("\n");
                     }
                     AuxExportKind::Constructor(class) => {
+                        let docs = format_doc_comments(&export.comments, Some(js_doc));
                         let exported = require_class(&mut self.exported_classes, class);
                         if exported.has_constructor {
                             bail!("found duplicate constructor for class `{}`", class);
@@ -2543,6 +2545,7 @@ impl<'a> Context<'a> {
                         exported.push(&docs, "constructor", "", &code, ts_sig);
                     }
                     AuxExportKind::Getter { class, field, .. } => {
+                        let docs = format_doc_comments(&export.comments, None);
                         let ret_ty = match export.generate_typescript {
                             true => match &ts_ret_ty {
                                 Some(s) => Some(s.as_str()),
@@ -2554,6 +2557,7 @@ impl<'a> Context<'a> {
                         exported.push_getter(&docs, field, &code, ret_ty);
                     }
                     AuxExportKind::Setter { class, field, .. } => {
+                        let docs = format_doc_comments(&export.comments, None);
                         let arg_ty = match export.generate_typescript {
                             true => Some(ts_arg_tys[0].as_str()),
                             false => None,
@@ -2562,10 +2566,12 @@ impl<'a> Context<'a> {
                         exported.push_setter(&docs, field, &code, arg_ty, might_be_optional_field);
                     }
                     AuxExportKind::StaticFunction { class, name } => {
+                        let docs = format_doc_comments(&export.comments, Some(js_doc));
                         let exported = require_class(&mut self.exported_classes, class);
                         exported.push(&docs, name, "static ", &code, ts_sig);
                     }
                     AuxExportKind::Method { class, name, .. } => {
+                        let docs = format_doc_comments(&export.comments, Some(js_doc));
                         let exported = require_class(&mut self.exported_classes, class);
                         exported.push(&docs, name, "", &code, ts_sig);
                     }

crates/cli-support/src/wit/mod.rs
@@ -806,7 +806,7 @@ impl<'a> Context<'a> {
                 arguments: vec![Descriptor::I32],
                 shim_idx: 0,
                 ret: descriptor.clone(),
-                inner_ret: None,
+                inner_ret: Some(descriptor.clone()),
             };
             let getter_id = self.export_adapter(getter_id, getter_descriptor)?;
             self.aux.export_map.insert(

crates/cli-support/src/js/binding.rs
@@ -229,6 +229,7 @@ impl<'a, 'b> Builder<'a, 'b> {
             asyncness,
         );
         let js_doc = self.js_doc_comments(&function_args, &arg_tys, &ts_ret_ty);
+
         Ok(JsFunction {
             code,
             ts_sig,

crates/typescript-tests/src/getters_setters.rs
@@ -66,3 +66,22 @@ impl ColorWithGetterAndSetter {
         };
     }
 }
+
+#[wasm_bindgen]
+pub struct ColorWithReadonly {
+    #[wasm_bindgen(readonly)]
+    pub r: f64,
+    #[wasm_bindgen(readonly)]
+    pub g: f64,
+    #[wasm_bindgen(readonly)]
+    pub b: f64,
+    pub a: u8,
+}
+
+#[wasm_bindgen]
+impl ColorWithReadonly {
+    #[wasm_bindgen(constructor)]
+    pub fn new(r: f64, g: f64, b: f64) -> ColorWithReadonly {
+        Self { r, b, g, a: 0 }
+    }
+}

crates/typescript-tests/src/getters_setters.ts
@@ -9,3 +9,7 @@ colorWithSetter.r = 1;
 const colorWithGetterAndSetter: wbg.ColorWithGetterAndSetter = new wbg.ColorWithGetterAndSetter;
 colorWithGetterAndSetter.r = 1;
 const _b = colorWithGetterAndSetter.r;
+
+const colorWithReadonly: wbg.ColorWithReadonly = new wbg.ColorWithReadonly(1, 2, 3);
+const _r: number = colorWithReadonly.r;
+colorWithReadonly.a = 4;

commit d529443c44e70ac4cbcb7797a4a069f9f6b1b2f0

fix(cli-support) Differentiate JS docs and TS docs when generating adapters.
A bug has been raised in
https://github.com/rustwasm/wasm-bindgen/issues/2947, which is the
consequence of a bug introduced in
https://github.com/rustwasm/wasm-bindgen/pull/2909.

This patch fixes it by differentiating JS docs and TS docs
clearly. The `Context::push_setter` and `Context::push_getter` methods
now receive a `js_docs` and a `ts_docs`, which prevent any confusions.

crates/cli-support/src/js/mod.rs
@@ -2564,31 +2564,33 @@ impl<'a> Context<'a> {
                     false => None,
                 };
 
+                let js_docs = format_doc_comments(&export.comments, Some(js_doc));
+
                 match &export.kind {
                     AuxExportKind::Function(name) => {
-                        let docs = format_doc_comments(&export.comments, Some(js_doc));
-
                         if let Some(ts_sig) = ts_sig {
-                            self.typescript.push_str(&docs);
+                            self.typescript.push_str(&js_docs);
                             self.typescript.push_str("export function ");
                             self.typescript.push_str(&name);
                             self.typescript.push_str(ts_sig);
                             self.typescript.push_str(";\n");
                         }
-                        self.export(&name, &format!("function{}", code), Some(&docs))?;
+
+                        self.export(&name, &format!("function{}", code), Some(&js_docs))?;
                         self.globals.push_str("\n");
                     }
                     AuxExportKind::Constructor(class) => {
-                        let docs = format_doc_comments(&export.comments, Some(js_doc));
                         let exported = require_class(&mut self.exported_classes, class);
+
                         if exported.has_constructor {
                             bail!("found duplicate constructor for class `{}`", class);
                         }
+
                         exported.has_constructor = true;
-                        exported.push(&docs, "constructor", "", &code, ts_sig);
+                        exported.push(&js_docs, "constructor", "", &code, ts_sig);
                     }
                     AuxExportKind::Getter { class, field, .. } => {
-                        let docs = format_doc_comments(&export.comments, None);
+                        let ts_docs = format_doc_comments(&export.comments, None);
                         let ret_ty = match export.generate_typescript {
                             true => match &ts_ret_ty {
                                 Some(s) => Some(s.as_str()),
@@ -2596,27 +2598,34 @@ impl<'a> Context<'a> {
                             },
                             false => None,
                         };
+
                         let exported = require_class(&mut self.exported_classes, class);
-                        exported.push_getter(&docs, field, &code, ret_ty);
+                        exported.push_getter(&js_docs, &ts_docs, field, &code, ret_ty);
                     }
                     AuxExportKind::Setter { class, field, .. } => {
-                        let docs = format_doc_comments(&export.comments, None);
+                        let ts_docs = format_doc_comments(&export.comments, None);
                         let arg_ty = match export.generate_typescript {
                             true => Some(ts_arg_tys[0].as_str()),
                             false => None,
                         };
                         let exported = require_class(&mut self.exported_classes, class);
-                        exported.push_setter(&docs, field, &code, arg_ty, might_be_optional_field);
+
+                        exported.push_setter(
+                            &js_docs,
+                            &ts_docs,
+                            field,
+                            &code,
+                            arg_ty,
+                            might_be_optional_field,
+                        );
                     }
                     AuxExportKind::StaticFunction { class, name } => {
-                        let docs = format_doc_comments(&export.comments, Some(js_doc));
                         let exported = require_class(&mut self.exported_classes, class);
-                        exported.push(&docs, name, "static ", &code, ts_sig);
+                        exported.push(&js_docs, name, "static ", &code, ts_sig);
                     }
                     AuxExportKind::Method { class, name, .. } => {
-                        let docs = format_doc_comments(&export.comments, Some(js_doc));
                         let exported = require_class(&mut self.exported_classes, class);
-                        exported.push(&docs, name, "", &code, ts_sig);
+                        exported.push(&js_docs, name, "", &code, ts_sig);
                     }
                 }
             }
@@ -3836,11 +3845,20 @@ impl ExportedClass {
 
     /// Used for adding a getter to a class, mainly to ensure that TypeScript
     /// generation is handled specially.
-    fn push_getter(&mut self, docs: &str, field: &str, js: &str, ret_ty: Option<&str>) {
-        self.push_accessor(docs, field, js, "get ");
+    fn push_getter(
+        &mut self,
+        js_docs: &str,
+        ts_docs: &str,
+        field: &str,
+        js: &str,
+        ret_ty: Option<&str>,
+    ) {
+        self.push_accessor(js_docs, field, js, "get ");
+
         if let Some(ret_ty) = ret_ty {
-            self.push_accessor_ts(docs, field, ret_ty, false);
+            self.push_accessor_ts(ts_docs, field, ret_ty, false);
         }
+
         self.readable_properties.push(field.to_string());
     }
 
@@ -3848,15 +3866,17 @@ impl ExportedClass {
     /// generation is handled specially.
     fn push_setter(
         &mut self,
-        docs: &str,
+        js_docs: &str,
+        ts_docs: &str,
         field: &str,
         js: &str,
         ret_ty: Option<&str>,
         might_be_optional_field: bool,
     ) {
-        self.push_accessor(docs, field, js, "set ");
+        self.push_accessor(js_docs, field, js, "set ");
+
         if let Some(ret_ty) = ret_ty {
-            let is_optional = self.push_accessor_ts(docs, field, ret_ty, true);
+            let is_optional = self.push_accessor_ts(ts_docs, field, ret_ty, true);
             *is_optional = might_be_optional_field;
         }
     }