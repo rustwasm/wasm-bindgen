b64c58c279e59478c4c0573d5d640d6433145f16
joaofreires <rmjoaovictor@gmail.com>
2022-06-21 17:01:59 -0300

    add variadic TS tests

--- crates/typescript-tests/src/simple_fn.ts
+++ crates/typescript-tests/src/simple_fn.ts
@@ -4,3 +4,5 @@ import * as wasm from '../pkg/typescript_tests_bg.wasm';
 const wbg_greet: (a: string) => void = wbg.greet;
 const wasm_greet: (a: number, b: number) => void = wasm.greet;
 const take_and_return_bool: (a: boolean) => boolean = wbg.take_and_return_bool;
+const wbg_variadic_function: (...arr: any) => any = wbg.variadic_function;
+const wasm_variadic_function: (arr: number) => number = wasm.variadic_function;
