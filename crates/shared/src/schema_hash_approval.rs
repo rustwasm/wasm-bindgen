59883eaca6c48a0d96303f04b92ac0e4d40a717d
GalvinGao <me@galvingao.com>
2022-07-25 23:06:53 +0800

    Bump to 0.2.82 (#3002)
    * Bump to 0.2.82
    
    * chore: bump version in crates/shared/src/lib.rs in sync with crates/shared/Cargo.toml
    
    * fix: update schema file hash

--- crates/shared/src/schema_hash_approval.rs
+++ crates/shared/src/schema_hash_approval.rs
@@ -8,7 +8,7 @@
 // If the schema in this library has changed then:
 //  1. Bump the version in `crates/shared/Cargo.toml`
 //  2. Change the `SCHEMA_VERSION` in this library to this new Cargo.toml version
-const APPROVED_SCHEMA_FILE_HASH: &'static str = "16056751188521403565";
+const APPROVED_SCHEMA_FILE_HASH: &'static str = "18229224525184165582";
 
 #[test]
 fn schema_version() {
