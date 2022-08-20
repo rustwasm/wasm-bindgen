b54791b751857d3e9ec480ad35bcdd0a399d3ad6
Lukas Lihotzki <lukas@lihotzki.de>
2022-08-20 16:40:35 +0200

    fix

--- crates/backend/src/encode.rs
+++ crates/backend/src/encode.rs
@@ -239,18 +239,22 @@ fn shared_variant<'a>(v: &'a ast::Variant, intern: &'a Interner) -> EnumVariant<
 
 fn shared_import<'a>(i: &'a ast::Import, intern: &'a Interner) -> Result<Import<'a>, Diagnostic> {
     Ok(Import {
-        module: match &i.module {
-            Some(ast::ImportModule::Named(m, span)) => {
-                Some(ImportModule::Named(intern.resolve_import_module(m, *span)?))
-            }
-            Some(ast::ImportModule::RawNamed(m, _span)) => {
-                Some(ImportModule::RawNamed(intern.intern_str(m)))
-            }
-            Some(ast::ImportModule::Inline(idx, _)) => Some(ImportModule::Inline(*idx as u32)),
-            None => None,
-        },
+        module: Option::transpose(i.module.as_ref().map(|m| shared_module(m, intern)))?,
         js_namespace: i.js_namespace.clone(),
         kind: shared_import_kind(&i.kind, intern)?,
+    })
+}
+
+fn shared_module<'a>(
+    m: &'a ast::ImportModule,
+    intern: &'a Interner,
+) -> Result<ImportModule<'a>, Diagnostic> {
+    Ok(match m {
+        ast::ImportModule::Named(m, span) => {
+            ImportModule::Named(intern.resolve_import_module(m, *span)?)
+        }
+        ast::ImportModule::RawNamed(m, _span) => ImportModule::RawNamed(intern.intern_str(m)),
+        ast::ImportModule::Inline(idx, _) => ImportModule::Inline(*idx as u32),
     })
 }
 
