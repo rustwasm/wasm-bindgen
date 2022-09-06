54f7b42090a2e2fc3bb7283dc62574b61f79a618
dAxpeDDa <daxpedda@gmail.com>
2022-09-06 14:00:32 +0200

    Fix `__wbindgen_main` being cleaned up

--- crates/cli-support/src/wit/mod.rs
+++ crates/cli-support/src/wit/mod.rs
@@ -1459,7 +1459,7 @@ impl<'a> Context<'a> {
         let mut to_remove = Vec::new();
         for export in self.module.exports.iter() {
             match export.name.as_str() {
-                n if n.starts_with("__wbindgen") => {
+                n if n.starts_with("__wbindgen") && n != "__wbindgen_main" => {
                     to_remove.push(export.id());
                 }
                 _ => {}
