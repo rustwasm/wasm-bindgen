b8b13630237ce698cd5bb212c14b548a499a92cb
Lukas Lihotzki <lukas@lihotzki.de>
2022-09-24 21:38:59 +0200

    Add tests

---
+++ crates/macro/ui-tests/link-to.rs
@@ -0,0 +1,24 @@
+fn good1() -> String {
+    wasm_bindgen::link_to!(inline_js = "console.log('Hello world!');")
+}
+
+fn good2() -> String {
+    wasm_bindgen::link_to!(raw_module = "package/foo.js")
+}
+
+fn bad1() -> String {
+    wasm_bindgen::link_to!(module = "/src/not-found.js")
+}
+
+fn bad2() -> String {
+    wasm_bindgen::link_to!()
+}
+
+fn bad3() -> String {
+    wasm_bindgen::link_to!(
+        inline_js = "console.log('Hello world!');",
+        js_namespace = foo
+    )
+}
+
+fn main() {}
