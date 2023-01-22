eb72dbccb105c2da6fea352e6742ca02c28f7be4
Liam Murphy <liampm32@gmail.com>
2023-01-22 17:55:02 +1100

    Update reference tests

--- crates/cli/tests/reference/string-arg.js
+++ crates/cli/tests/reference/string-arg.js
@@ -10,10 +10,10 @@ let cachedTextDecoder = new lTextDecoder('utf-8', { ignoreBOM: true, fatal: true
 
 cachedTextDecoder.decode();
 
-let cachedUint8Memory0 = new Uint8Array();
+let cachedUint8Memory0 = null;
 
 function getUint8Memory0() {
-    if (cachedUint8Memory0.byteLength === 0) {
+    if (cachedUint8Memory0 === null || cachedUint8Memory0.byteLength === 0) {
         cachedUint8Memory0 = new Uint8Array(wasm.memory.buffer);
     }
     return cachedUint8Memory0;
