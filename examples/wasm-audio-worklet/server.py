cd329506694078c46206df8de6498245508dfc75
chaosprint <chaosprint@gmail.com>
2023-03-10 11:26:52 +0100

    fix audioworklet example local server error

--- examples/wasm-audio-worklet/server.py
+++ examples/wasm-audio-worklet/server.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-from http.server import HTTPServer, SimpleHTTPRequestHandler, test
+from http.server import HTTPServer, SimpleHTTPRequestHandler
 import sys
 
 class RequestHandler(SimpleHTTPRequestHandler):
@@ -9,4 +9,7 @@ class RequestHandler(SimpleHTTPRequestHandler):
         SimpleHTTPRequestHandler.end_headers(self)
 
 if __name__ == '__main__':
-    test(RequestHandler, HTTPServer, port=int(sys.argv[1]) if len(sys.argv) > 1 else 8000)
+    port = int(sys.argv[1]) if len(sys.argv) > 1 else 8080
+    httpd = HTTPServer(('localhost', port), RequestHandler)
+    print("http://localhost:8080")
+    httpd.serve_forever()
