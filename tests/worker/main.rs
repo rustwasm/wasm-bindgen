b51acbf73a67fc1ced532888588f6ff678a3fb58
WhizSid <whizsid@aol.com>
2023-04-13 19:40:55 +0530

    Removed memory module from worker tests

--- tests/worker/main.rs
+++ tests/worker/main.rs
@@ -9,7 +9,6 @@ use wasm_bindgen_test::{wasm_bindgen_test, wasm_bindgen_test_configure};
 
 wasm_bindgen_test_configure!(run_in_worker);
 
-pub mod memory;
 pub mod modules;
 
 // should not be executed
