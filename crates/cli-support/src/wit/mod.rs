c79b2d562535cf1b659424bbb1050b68894ddb55
Oliver T <geronimooliver00@gmail.com>
2023-05-15 09:48:17 -0400

    Improved version mismatch error message
    Fixes #2619
    
    Signed-off-by: Oliver T <geronimooliver00@gmail.com>

--- crates/cli-support/src/wit/mod.rs
+++ crates/cli-support/src/wit/mod.rs
@@ -1479,16 +1479,17 @@ version of wasm-bindgen that uses a different bindgen format than this binary:
      this binary schema version: {}
 
 Currently the bindgen format is unstable enough that these two schema versions
-must exactly match. You can accomplish this by either updating the wasm-bindgen
-dependency or this binary.
+must exactly match. You can accomplish this by either updating this binary or 
+the wasm-bindgen dependency in the Rust project.
 
 You should be able to update the wasm-bindgen dependency with:
 
-    cargo update -p wasm-bindgen
+    cargo update -p wasm-bindgen --precise {my_version}
 
-or you can update the binary with
+don't forget to recompile your wasm file! Alternatively, you can update the 
+binary with:
 
-    cargo install -f wasm-bindgen-cli
+    cargo install -f wasm-bindgen-cli --version {their_version}
 
 if this warning fails to go away though and you're not sure what to do feel free
 to open an issue at https://github.com/rustwasm/wasm-bindgen/issues!
