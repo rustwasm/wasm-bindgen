30df8e84df0063a1fbddc7665c5e64fc689d189f
Sammy Harris <sammy@stegabob.com>
2023-05-29 15:17:03 -0400

    update fixed size adaptor/ix defs

--- crates/cli-support/src/wit/standard.rs
+++ crates/cli-support/src/wit/standard.rs
@@ -87,7 +87,7 @@ pub enum AdapterType {
     Struct(String),
     NamedExternref(String),
     Function,
-    Array(Box<AdapterType>, Box<AdapterType>)
+    Array(Box<AdapterType>, usize),
 }
 
 #[derive(Debug, Clone)]
@@ -278,7 +278,7 @@ pub enum Instruction {
     },
     ArrayLoad {
         kind: AdapterType,
-        length: AdapterType,
+        length: usize,
     },
     /// pops i32, loads externref from externref table
     TableGet,
