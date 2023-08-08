486238b524633d3109ae794c40ca96f7fa64d5a9
Oliver <geronimooliver00@gmail.com>
2023-08-08 19:43:55 -0400

    Add missing CSS loaders for todomvc app (#3535)
    * Add missing CSS loaders for todomvc app
    
    Currently webpack does not bundle the index.css file located in the root
    directory. This PR adds the corresponding plugins and loaders to ensure
    the file is correctly bundled.
    
    Fixes #3443
    
    * Fixed failing CI build
    
    Bumped version of `css-loader` as we're on webpack 5 already.
    
    Signed-off-by: Oliver T <geronimooliver00@gmail.com>
    
    ---------
    
    Signed-off-by: Oliver T <geronimooliver00@gmail.com>

--- examples/todomvc/index.js
+++ examples/todomvc/index.js
@@ -1,6 +1,7 @@
 // For more comments about what's going on here, check out the `hello_world`
 // example
 const rust = import('./pkg');
+const css = import('./index.css');
 
 rust
   .then(m => m.run())
