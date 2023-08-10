a5c9fadb7e3bcfb383ade08830cd3a4a91924642
Liam Murphy <liampm32@gmail.com>
2023-08-10 21:28:54 +1000

    Update schema hash
    I didn't actually bump the schema version because it didn't change. If you don't use the `TryFrom<JsValue>` impl for Rust structs (or pass a `Vec` of them from JS to Rust), using an old CLI version will work fine; if you do though, you get a bit of a cryptic error message:
    
    ```
    error: import of `__wbg_foo_unwrap` doesn't have an adapter listed
    ```
    
    (That's from trying to pass a `Vec<Foo>` from JS to Rust.)
    
    So idk, maybe that's worth bumping the schema version over anyway?

--- crates/shared/src/schema_hash_approval.rs
+++ crates/shared/src/schema_hash_approval.rs
@@ -8,7 +8,7 @@
 // If the schema in this library has changed then:
 //  1. Bump the version in `crates/shared/Cargo.toml`
 //  2. Change the `SCHEMA_VERSION` in this library to this new Cargo.toml version
-const APPROVED_SCHEMA_FILE_HASH: &str = "12040133795598472740";
+const APPROVED_SCHEMA_FILE_HASH: &str = "5679641936258023729";
 
 #[test]
 fn schema_version() {
