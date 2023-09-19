f76f20ee54e581c0456ae14650e378248363dea7
Oliver T <geronimooliver00@gmail.com>
2023-09-19 17:37:20 -0400

    Unwrap argument types in macro
    `syn` wraps argument types in a `syn::TypeGroup` for free
    functions/methods generated from `macro_rules!` this means that
    references end up generating wrong code during proc macro expansion.
    
    This PR unwraps argument types in `syn::TypeGroup` and matches against
    the inner element for such cases.
    
    Fixes #3078
    
    Signed-off-by: Oliver T <geronimooliver00@gmail.com>

--- crates/backend/src/codegen.rs
+++ crates/backend/src/codegen.rs
@@ -523,8 +523,12 @@ impl TryToTokens for ast::Export {
             argtys.push(&*arg.ty);
             let i = i + offset;
             let ident = Ident::new(&format!("arg{}", i), Span::call_site());
-            let ty = &arg.ty;
-            match &*arg.ty {
+            let ty = match &*arg.ty { 
+                syn::Type::Group(syn::TypeGroup { ref elem, .. }) => elem,
+                _ => &arg.ty,
+            };
+
+            match &**ty {
                 syn::Type::Reference(syn::TypeReference {
                     mutability: Some(_),
                     elem,
