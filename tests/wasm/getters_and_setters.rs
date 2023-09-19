bdf9408d099a03866f5f13b4511747561901ec36
Solo-steven <ssteven19@kimo.com>
2023-09-19 16:13:29 +0800

    fix/test: add test case and use CI pipeline for running it

--- tests/wasm/getters_and_setters.rs
+++ tests/wasm/getters_and_setters.rs
@@ -20,6 +20,8 @@ extern "C" {
     fn _12_js(rules: Rules) -> Rules;
     fn _13_js(rules: Rules) -> Rules;
 
+    fn raw_identifer(rules: RulesWithRawField) -> RulesWithRawField;
+
     fn test_getter_compute(x: GetterCompute);
     fn test_setter_compute(x: SetterCompute);
     fn test_statics(x: Statics);
@@ -30,6 +32,22 @@ extern "C" {
 #[wasm_bindgen]
 pub struct Rules {
     pub field: i32,
+}
+
+#[wasm_bindgen]
+pub struct RulesWithRawField {
+    pub r#field: i32,
+}
+
+impl RulesWithRawField {
+    #[wasm_bindgen]
+    pub fn get_field_value(&self) -> i32 {
+        self.r#field
+    }
+    #[wasm_bindgen]
+    pub fn set_field_value(&self, value: i32) {
+        self.r#field = value;
+    }
 }
 
 #[wasm_bindgen]
