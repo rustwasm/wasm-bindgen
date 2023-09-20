8c59a67895895b1eb0bf0cc15007b4e2ed0d15be
Oliver T <geronimooliver00@gmail.com>
2023-09-20 13:22:40 -0400

    fix clippy
    Signed-off-by: Oliver T <geronimooliver00@gmail.com>

--- tests/wasm/macro_rules.rs
+++ tests/wasm/macro_rules.rs
@@ -1,12 +1,11 @@
 //! This tests that the `wasm_bindgen` macro produces code that compiles for this use case.
 //! `cargo test --target wasm32-unknown-unknown` will not run if this test breaks.
 use wasm_bindgen::prelude::*;
-use wasm_bindgen_test::*;
 
 macro_rules! my_export {
     ($i: ident, $s: ty) => {
         #[wasm_bindgen]
-        pub fn $i(s: $s) {}
+        pub fn $i(_: $s) {}
     };
 }
 
