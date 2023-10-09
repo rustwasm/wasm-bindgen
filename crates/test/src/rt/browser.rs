4e609520b3bf2f96624e07b0a60b5931bacdb0e7
daxpedda <daxpedda@gmail.com>
2023-10-09 12:14:50 +0200

    Handle `#[ignore = "reason"]` on tests (#3644)

--- crates/test/src/rt/browser.rs
+++ crates/test/src/rt/browser.rs
@@ -6,6 +6,8 @@
 use js_sys::Error;
 use wasm_bindgen::prelude::*;
 
+use super::TestResult;
+
 /// Implementation of `Formatter` for browsers.
 ///
 /// Routes all output to a `pre` on the page currently. Eventually this probably
@@ -49,9 +51,8 @@ impl super::Formatter for Browser {
         self.pre.set_text_content(&html);
     }
 
-    fn log_test(&self, name: &str, result: &Result<(), JsValue>) {
-        let s = if result.is_ok() { "ok" } else { "FAIL" };
-        self.writeln(&format!("test {} ... {}", name, s));
+    fn log_test(&self, name: &str, result: &TestResult) {
+        self.writeln(&format!("test {} ... {}", name, result));
     }
 
     fn stringify_error(&self, err: &JsValue) -> String {
