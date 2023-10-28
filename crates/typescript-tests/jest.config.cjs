6992750c24696b645aade40ebce0914ac0e8f1a3
Thomas Etter <etterth@gmail.com>
2023-10-28 23:54:01 +0200

    Add jest for simple_struct.ts, this causes it to fail

---
+++ crates/typescript-tests/jest.config.cjs
@@ -0,0 +1,15 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  preset: "ts-jest/presets/default-esm",
+  testEnvironment: 'node',
+  extensionsToTreatAsEsm: [".ts"],
+  verbose: true,
+  testMatch: ['**/src/simple_struct.ts'],
+  globals: {
+    'ts-jest':
+    {
+      useESM: true,
+      isolatedModules: true
+    }
+  }
+};
