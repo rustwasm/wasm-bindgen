cfe3dc28a101a1b4cf7ab7ca6db358eadc7a4063
Thomas Etter <etterth@gmail.com>
2023-11-30 18:52:20 +0100

    Add a test runner to two of the typescript tests to actually run them (#3704)
    * Add jest for simple_struct.ts, this causes it to fail
    
    * fix the test, add another failing one
    
    * Fix the typescript_type test
    
    * do not use jest global injection
    
    * add TODO for the rest of the tests

--- crates/typescript-tests/src/simple_struct.ts
+++ crates/typescript-tests/src/simple_struct.ts
@@ -1,9 +1,20 @@
-import * as wbg from '../pkg/typescript_tests';
+import * as wbg from "../pkg/typescript_tests";
+import { expect, jest, test } from "@jest/globals";
 
-const a = new wbg.A();
-wbg.A.other();
-a.foo();
-a.free();
-const b: boolean = a.ret_bool()
-a.take_bool(b);
-a.take_many(b, 1, 2);
+test("member function (void) -> void", () => {
+  const a = new wbg.A();
+  wbg.A.other();
+  a.foo();
+  a.free();
+  expect(() => {
+    a.ret_bool();
+  }).toThrow(/null pointer passed to rust/);
+});
+
+test("function with parameters", () => {
+  const a = new wbg.A();
+  const b: boolean = a.ret_bool();
+  expect(b).toStrictEqual(true);
+  a.take_bool(b);
+  a.take_many(b, 1, 2);
+});
--- crates/typescript-tests/src/typescript_type.ts
+++ crates/typescript-tests/src/typescript_type.ts
@@ -1,9 +1,30 @@
-import * as wbg from '../pkg/typescript_tests';
+import * as wbg from "../pkg/typescript_tests";
+import { expect, jest, test } from "@jest/globals";
 
-const style: wbg.TextStyle = new wbg.TextStyle({
-  bold: true,
-  italic: true,
-  size: 42,
+test("constructor", () => {
+  const style: wbg.TextStyle = new wbg.TextStyle({
+    bold: true,
+    italic: false,
+    size: 42,
+  });
+
+  expect(style.bold).toStrictEqual(true);
+  expect(style.italic).toStrictEqual(false);
+  expect(style.size).toStrictEqual(42);
 });
 
-const optional_style: wbg.TextStyle = wbg.TextStyle.optional_new();
+test("optional parameter constructor", () => {
+  const default_constructed: wbg.TextStyle = wbg.TextStyle.optional_new();
+  expect(default_constructed.bold).toStrictEqual(false);
+  expect(default_constructed.italic).toStrictEqual(false);
+  expect(default_constructed.size).toStrictEqual(0);
+
+  const optional_style: wbg.TextStyle = wbg.TextStyle.optional_new({
+    italic: true,
+    bold: false,
+    size: 0,
+  });
+  expect(optional_style.bold).toStrictEqual(false);
+  expect(optional_style.italic).toStrictEqual(true);
+  expect(optional_style.size).toStrictEqual(0);
+});
