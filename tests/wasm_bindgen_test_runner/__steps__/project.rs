2ee9f02442bfb1b26e98cb6709b9c3d3e36b1f91
Alexandre Faria <alexandre.faria@auroka.pt>
2024-03-28 23:07:12 +0000

    wasm-bindgen-test-runner: Updated to build the assembly with a proper cargo test command.

--- tests/wasm_bindgen_test_runner/__steps__/project.rs
+++ tests/wasm_bindgen_test_runner/__steps__/project.rs
@@ -1,10 +1,11 @@
-use assert_cmd::prelude::*;
 use predicates::str;
 use rand::Rng;
+use regex::Regex;
 use std::env;
 use std::fs;
 use std::path::PathBuf;
 use std::process::Command;
+use std::process::Output;
 
 fn target_dir() -> PathBuf {
     let mut dir = env::current_exe().unwrap();
@@ -28,14 +29,19 @@ pub struct Project {
 impl Project {
     pub fn new(name: &'static str) -> Project {
         let mut rng = rand::thread_rng();
-        let name = format!("{}_{}", name, rng.gen_range(1000..9999));
         let root = target_dir()
             .join("wasm-bindgen-test-runner-tests")
-            .join(&name);
-        println!("root: {:?}", root);
+            .join(format!("{}_{}", name, rng.gen_range(1000..9999)));
         drop(fs::remove_dir_all(&root));
         fs::create_dir_all(&root).unwrap();
-        Project { root, name }
+        Project {
+            root,
+            name: name.to_string(),
+        }
+    }
+
+    pub fn clean(&mut self) {
+        drop(fs::remove_dir_all(&self.root));
     }
 
     pub fn file(&mut self, name: &str, contents: &str) -> &mut Project {
@@ -45,59 +51,55 @@ impl Project {
         self
     }
 
-    fn wasm_bindgen(&mut self, args: &str) -> (Command, PathBuf) {
-        let wasm = self.build();
-        let output = self.root.join("pkg");
-        fs::create_dir_all(&output).unwrap();
-        let mut cmd = Command::cargo_bin("wasm-bindgen").unwrap();
-        cmd.arg("--out-dir").arg(&output);
-        cmd.arg(&wasm);
-        for arg in args.split_whitespace() {
-            cmd.arg(arg);
-        }
-        (cmd, output)
-    }
-
     pub fn build(&mut self) -> PathBuf {
         if !self.root.join("Cargo.toml").is_file() {
             self.file(
                 "Cargo.toml",
                 &format!(
-                    "
-                        [package]
-                        name = \"{}\"
-                        authors = []
-                        version = \"1.0.0\"
-                        edition = '2021'
+                    "[package]
+name = '{}'
+authors = []
+version = '1.0.0'
+edition = '2021'
 
-                        [dependencies]
-                        wasm-bindgen = {{ path = '{}' }}
+[dev-dependencies]
+wasm-bindgen-test = {{ path = '{}/crates/test' }}
 
-                        [lib]
-                        crate-type = ['cdylib']
+[patch.crates-io]
+wasm-bindgen = {{ path = '{}' }}
 
-                        [workspace]
-                    ",
+[workspace]
+",
                     self.name,
+                    repo_root().display(),
                     repo_root().display(),
                 ),
             );
         }
 
-        let target_dir = target_dir();
-        Command::new("cargo")
+        let output = Command::new("cargo")
             .current_dir(&self.root)
-            .arg("build")
+            .arg("test")
             .arg("--target")
             .arg("wasm32-unknown-unknown")
-            .env("CARGO_TARGET_DIR", &target_dir)
-            .assert()
-            .success();
+            .arg("--no-run")
+            .output()
+            .expect("Failed to build test assembly");
 
-        target_dir
-            .join("wasm32-unknown-unknown")
-            .join("debug")
-            .join(&self.name)
-            .with_extension("wasm")
+        let assembly = extract_assembly_from_output(output);
+
+        self.root.join(assembly)
     }
+}
+
+fn extract_assembly_from_output(output: Output) -> String {
+    let error_str = std::str::from_utf8(&output.stderr).unwrap();
+    let last = error_str.lines().last().unwrap();
+
+    let re = Regex::new(r"\((.*?)\)").unwrap();
+    let captures = re
+        .captures(last)
+        .expect("Failed to find generated assembly");
+
+    captures.get(1).unwrap().as_str().to_string()
 }
