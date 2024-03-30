46e38764468d81baa0a920d749ecb8c659393cd1
Alexandre Faria <alexandre.faria@auroka.pt>
2024-03-30 23:37:38 +0000

    wasm-bindgen-test-runner-test: Added feature Outputs the assembly failure summary to invocation with_an_assembly with_one_failing_test.

--- tests/wasm_bindgen_test_runner/__features__/invocation/with_an_assembly/with_one_failing_test/outputs_the_assembly_failure_summary_feature.rs
+++ tests/wasm_bindgen_test_runner/__features__/invocation/with_an_assembly/with_one_failing_test/outputs_the_assembly_failure_summary_feature.rs
@@ -4,9 +4,12 @@ use crate::__steps__::wasm_bindgen_test_runner::when_wasm_bindgen_test_runner_is
 use crate::__steps__::Context;
 
 #[test]
-fn outputs_the_failed_test_summary_feature() {
+fn outputs_the_assembly_failure_summary_feature() {
     let mut context = Context::new();
     given_there_is_an_assembly_with_one_failing_test(&mut context);
     when_wasm_bindgen_test_runner_is_invoked_with_the_assembly(&mut context);
-    then_the_standard_output_should_have(context, "test assembly_with_one_failing_test::fail ... FAIL");
+    then_the_standard_output_should_have(
+        context,
+        "failures:\n\n    assembly_with_one_failing_test::fail",
+    );
 }
