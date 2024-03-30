0eeeb17a10d9225c150d8a504def8a055e3da73b
Alexandre Faria <alexandre.faria@auroka.pt>
2024-03-30 23:10:57 +0000

    wasm-bindgen-test-runner-test: Improved features names of invocation with_an_assembly with_one_successful_and_one_failing_test.

--- tests/wasm_bindgen_test_runner/__features__/invocation/with_an_assembly/with_one_successful_and_one_failing_test/outputs_the_successful_test_summary_feature.rs
+++ tests/wasm_bindgen_test_runner/__features__/invocation/with_an_assembly/with_one_successful_and_one_failing_test/outputs_the_successful_test_summary_feature.rs
@@ -4,7 +4,7 @@ use crate::__steps__::wasm_bindgen_test_runner::when_wasm_bindgen_test_runner_is
 use crate::__steps__::Context;
 
 #[test]
-fn outputs_failed_test_execution_feature() {
+fn outputs_failed_test_summary_feature() {
     let mut context = Context::new();
     given_there_is_an_assembly_with_one_successful_and_one_failing_test(&mut context);
     when_wasm_bindgen_test_runner_is_invoked_with_the_assembly(&mut context);
