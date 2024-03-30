06d130e681137468fe9e7e23306e38b6730c23da
Alexandre Faria <alexandre.faria@auroka.pt>
2024-03-30 23:14:43 +0000

    wasm-bindgen-test-runner-test: Added feature Outputs its running 1 test to invocation with_an_assembly with_one_successful_test.

--- tests/wasm_bindgen_test_runner/__features__/invocation/with_an_assembly/with_one_successful_test/outputs_successful_test_summary_feature.rs
+++ tests/wasm_bindgen_test_runner/__features__/invocation/with_an_assembly/with_one_successful_test/outputs_successful_test_summary_feature.rs
@@ -4,9 +4,9 @@ use crate::__steps__::wasm_bindgen_test_runner::when_wasm_bindgen_test_runner_is
 use crate::__steps__::Context;
 
 #[test]
-fn outputs_successful_test_execution_feature() {
+fn outputs_successful_test_summary_feature() {
     let mut context = Context::new();
     given_there_is_an_assembly_with_one_successful_test(&mut context);
     when_wasm_bindgen_test_runner_is_invoked_with_the_assembly(&mut context);
-    then_the_standard_output_should_have(context, "running 1 test\n\ntest assembly_with_one_successful_test::pass ... ok\n\ntest result: ok. 1 passed; 0 failed; 0 ignored; 0 filtered out");
+    then_the_standard_output_should_have(context, "test assembly_with_one_successful_test::pass ... ok\n\ntest result: ok. 1 passed; 0 failed; 0 ignored; 0 filtered out");
 }
