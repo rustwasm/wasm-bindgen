fe2d0580ebd95b9e2c582d1df1aebfb1c4122fae
async-rustacean <swnblovego@gmail.com>
2024-04-09 15:53:22 +0800

    chore(typescript-tests): rename custom_section_type to custom_section_type.d.ts

--- crates/typescript-tests/src/custom_section.rs
+++ crates/typescript-tests/src/custom_section.rs
@@ -6,7 +6,7 @@ const TS_INTERFACE_EXPORT: &'static str = r"
 ";
 
 #[wasm_bindgen(typescript_custom_section)]
-const TS_INTERFACE_EXPORT1: &'static str = include_str!("./custom_section_types");
+const TS_INTERFACE_EXPORT1: &'static str = include_str!("./custom_section_types.d.ts");
 
 const TS_INTERFACE_EXPORT2: &str = "interface Person2 { height: number; }";
 #[wasm_bindgen(typescript_custom_section)]
