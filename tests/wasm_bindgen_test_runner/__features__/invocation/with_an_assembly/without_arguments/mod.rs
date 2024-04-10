175c97440670cef977514fda3d672fbfb52d808f
Alexandre Faria <alexandre.faria@auroka.pt>
2024-04-10 17:16:51 +0100

    wasm-bindgen-test-runner: Added test feature Outputs the assembly test summary to invocation with_an_assembly without_arguments with_one_ignored_test.

--- tests/wasm_bindgen_test_runner/__features__/invocation/with_an_assembly/without_arguments/mod.rs
+++ tests/wasm_bindgen_test_runner/__features__/invocation/with_an_assembly/without_arguments/mod.rs
@@ -1,4 +1,5 @@
 mod with_one_failing_test;
+mod with_one_ignored_test;
 mod with_one_successful_and_one_failing_test;
 mod with_one_successful_test;
 mod without_tests;
--- tests/wasm_bindgen_test_runner/__steps__/assembly/mod.rs
+++ tests/wasm_bindgen_test_runner/__steps__/assembly/mod.rs
@@ -1,11 +1,13 @@
 mod assembly_builder;
 mod given_there_is_an_assembly_with_one_failing_test;
+mod given_there_is_an_assembly_with_one_ignored_test;
 mod given_there_is_an_assembly_with_one_successful_and_one_failing_test;
 mod given_there_is_an_assembly_with_one_successful_test;
 mod given_there_is_an_assembly_without_anything;
 
 pub use assembly_builder::*;
 pub use given_there_is_an_assembly_with_one_failing_test::*;
+pub use given_there_is_an_assembly_with_one_ignored_test::*;
 pub use given_there_is_an_assembly_with_one_successful_and_one_failing_test::*;
 pub use given_there_is_an_assembly_with_one_successful_test::*;
 pub use given_there_is_an_assembly_without_anything::*;
