c3c8e29b59a4fb1196141d0069ef495af28c9ac5
Alexandre Faria <alexandre.faria@auroka.pt>
2024-04-10 17:28:17 +0100

    wasm-bindgen-test-runner: Added test feature Outputs no error to invocation with_an_assembly without_arguments with_one_ignored_test.

---
+++ tests/wasm_bindgen_test_runner/__features__/invocation/with_an_assembly/without_arguments/with_one_ignored_test/outputs_no_error_feature.rs
@@ -0,0 +1,12 @@
+use crate::__steps__::assembly::given_there_is_an_assembly_with_one_ignored_test;
+use crate::__steps__::standard_error::then_the_standard_error_should_be_empty;
+use crate::__steps__::wasm_bindgen_test_runner::when_wasm_bindgen_test_runner_is_invoked_with_the_assembly;
+use crate::__steps__::Context;
+
+#[test]
+fn outputs_no_error_feature() {
+    let mut context = Context::new();
+    given_there_is_an_assembly_with_one_ignored_test(&mut context);
+    when_wasm_bindgen_test_runner_is_invoked_with_the_assembly(&mut context);
+    then_the_standard_error_should_be_empty(context);
+}
