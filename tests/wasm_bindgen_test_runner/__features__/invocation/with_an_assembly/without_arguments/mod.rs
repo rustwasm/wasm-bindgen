6dd3bad7a1f258f2bcd90fcec6accdcfc1502459
Alexandre Faria <alexandre.faria@auroka.pt>
2024-04-10 21:06:51 +0100

    wasm-bindgen-test-runner: Moved feature tets of invocation with_an_assemblyt without_arguments into level_0 to add tets for other levels.

--- tests/wasm_bindgen_test_runner/__features__/invocation/with_an_assembly/without_arguments/mod.rs
+++ tests/wasm_bindgen_test_runner/__features__/invocation/with_an_assembly/without_arguments/mod.rs
@@ -1,6 +1,1 @@
-mod with_one_failing_test;
-mod with_one_ignored_test;
-mod with_one_successful_and_one_failing_test;
-mod with_one_successful_test;
-mod with_one_successfull_and_one_ignored_test;
-mod without_tests;
+mod level_0;
