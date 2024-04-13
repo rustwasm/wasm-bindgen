386a526f2bf40d2b1d60c288d30c7d568257367f
Alexandre Faria <alexandre.faria@auroka.pt>
2024-04-13 21:54:10 +0100

    wasm-bindgen-test-runnner: Added test feature Outputs the test in the terse format to the invocation with_an_assembly with_arguments --list --format terse --ignored level_1 with_one_ignored_test.

---
+++ tests/wasm_bindgen_test_runner/__steps__/assembly/with_one_ignored_test/mod.rs
@@ -0,0 +1,5 @@
+mod given_there_is_an_assembly_with_one_ignored_level_1_test;
+mod given_there_is_an_assembly_with_one_ignored_test;
+
+pub use given_there_is_an_assembly_with_one_ignored_level_1_test::*;
+pub use given_there_is_an_assembly_with_one_ignored_test::*;
