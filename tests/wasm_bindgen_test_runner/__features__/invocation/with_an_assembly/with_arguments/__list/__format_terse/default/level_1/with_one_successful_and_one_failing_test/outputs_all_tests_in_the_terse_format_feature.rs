7414fba89d18b4651b770deec62920e1ffe15372
Alexandre Faria <alexandre.faria@auroka.pt>
2024-04-13 20:24:17 +0100

    wasm-bindgen-test-runner: Added feature test Outputs all tests in the terse format to invocation with_an_assembly with_arguments --list --format terse default level_1 with_one_successful_an_one_failing_test.

---
+++ tests/wasm_bindgen_test_runner/__features__/invocation/with_an_assembly/with_arguments/__list/__format_terse/default/level_1/with_one_successful_and_one_failing_test/outputs_all_tests_in_the_terse_format_feature.rs
@@ -0,0 +1,19 @@
+use crate::__steps__::assembly::given_there_is_an_assembly_with_one_successful_and_one_failing_level_1_tests;
+use crate::__steps__::standard_output::then_the_standard_output_should_have;
+use crate::__steps__::wasm_bindgen_test_runner::when_wasm_bindgen_test_runner_is_invoked_with_the_assembly_and_the_arguments;
+use crate::__steps__::Context;
+
+#[test]
+fn outputs_all_tests_in_the_terse_format_feature() {
+    let mut context = Context::new();
+    given_there_is_an_assembly_with_one_successful_and_one_failing_level_1_tests(&mut context);
+    when_wasm_bindgen_test_runner_is_invoked_with_the_assembly_and_the_arguments(
+        &mut context,
+        "--list --format terse",
+    );
+    then_the_standard_output_should_have(
+        context,
+        r#"level_1::pass: test
+level_1::fail: test"#,
+    );
+}
