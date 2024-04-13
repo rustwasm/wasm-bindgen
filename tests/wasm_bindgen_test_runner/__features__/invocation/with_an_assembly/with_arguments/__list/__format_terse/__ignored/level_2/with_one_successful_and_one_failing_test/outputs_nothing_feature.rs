3193035ab8e84ce1c2a49e826f1c4c267da89b86
Alexandre Faria <alexandre.faria@auroka.pt>
2024-04-13 22:05:44 +0100

    wasm-bindgen-test-runnner: Added test feature Outputs nothing to the invocation with_an_assembly with_arguments --list --format terse --ignored level_2 with_one_successful_and_one_failing_tests.

---
+++ tests/wasm_bindgen_test_runner/__features__/invocation/with_an_assembly/with_arguments/__list/__format_terse/__ignored/level_2/with_one_successful_and_one_failing_test/outputs_nothing_feature.rs
@@ -0,0 +1,15 @@
+use crate::__steps__::assembly::given_there_is_an_assembly_with_one_successful_and_one_failing_level_2_tests;
+use crate::__steps__::standard_output::then_the_standard_output_should_be_empty;
+use crate::__steps__::wasm_bindgen_test_runner::when_wasm_bindgen_test_runner_is_invoked_with_the_assembly_and_the_arguments;
+use crate::__steps__::Context;
+
+#[test]
+fn outputs_nothing_feature() {
+    let mut context = Context::new();
+    given_there_is_an_assembly_with_one_successful_and_one_failing_level_2_tests(&mut context);
+    when_wasm_bindgen_test_runner_is_invoked_with_the_assembly_and_the_arguments(
+        &mut context,
+        "--list --format terse --ignored",
+    );
+    then_the_standard_output_should_be_empty(context);
+}
