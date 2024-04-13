8ba7902f09197d592b1271f17df59865e7f05fab
Alexandre Faria <alexandre.faria@auroka.pt>
2024-04-13 22:12:05 +0100

    wasm-bindgen-test-runnner: Added test feature Outputs the test in the terse format to the invocation with_an_assembly with_arguments --list --format terse --ignored level_2 with_one_ignored_test.

---
+++ tests/wasm_bindgen_test_runner/__features__/invocation/with_an_assembly/with_arguments/__list/__format_terse/__ignored/level_2/with_one_ignored_test/outputs_the_test_in_the_terse_format_feature.rs
@@ -0,0 +1,15 @@
+use crate::__steps__::assembly::given_there_is_an_assembly_with_one_ignored_level_2_test;
+use crate::__steps__::standard_output::then_the_standard_output_should_have;
+use crate::__steps__::wasm_bindgen_test_runner::when_wasm_bindgen_test_runner_is_invoked_with_the_assembly_and_the_arguments;
+use crate::__steps__::Context;
+
+#[test]
+fn outputs_the_test_in_the_terse_format_feature() {
+    let mut context = Context::new();
+    given_there_is_an_assembly_with_one_ignored_level_2_test(&mut context);
+    when_wasm_bindgen_test_runner_is_invoked_with_the_assembly_and_the_arguments(
+        &mut context,
+        "--list --format terse --ignored",
+    );
+    then_the_standard_output_should_have(context, r#"level_1::level_2::ignored: test"#);
+}
--- tests/wasm_bindgen_test_runner/__steps__/assembly/with_one_ignored_test/mod.rs
+++ tests/wasm_bindgen_test_runner/__steps__/assembly/with_one_ignored_test/mod.rs
@@ -1,5 +1,7 @@
 mod given_there_is_an_assembly_with_one_ignored_level_1_test;
+mod given_there_is_an_assembly_with_one_ignored_level_2_test;
 mod given_there_is_an_assembly_with_one_ignored_test;
 
 pub use given_there_is_an_assembly_with_one_ignored_level_1_test::*;
+pub use given_there_is_an_assembly_with_one_ignored_level_2_test::*;
 pub use given_there_is_an_assembly_with_one_ignored_test::*;
