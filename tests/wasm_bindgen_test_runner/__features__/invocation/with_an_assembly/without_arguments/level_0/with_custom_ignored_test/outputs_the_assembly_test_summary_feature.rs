d465caa0d96842060c997f5a89e161b97e9c9576
Alexandre Faria <alexandre.faria@auroka.pt>
2024-04-14 21:46:18 +0100

    wasm-bindgen-test-runner: Added feature test Outputs the assembly test summary to the invocation with_an_assembly without_arguments level_0 with_custom_ignored_test.

---
+++ tests/wasm_bindgen_test_runner/__features__/invocation/with_an_assembly/without_arguments/level_0/with_custom_ignored_test/outputs_the_assembly_test_summary_feature.rs
@@ -0,0 +1,15 @@
+use crate::__steps__::assembly::given_there_is_an_assembly_with_one_custom_ignored_test;
+use crate::__steps__::standard_output::then_the_standard_output_should_have;
+use crate::__steps__::wasm_bindgen_test_runner::when_wasm_bindgen_test_runner_is_invoked_with_the_assembly;
+use crate::__steps__::Context;
+
+#[test]
+fn outputs_the_assembly_test_summary_feature() {
+    let mut context = Context::new();
+    given_there_is_an_assembly_with_one_custom_ignored_test(&mut context);
+    when_wasm_bindgen_test_runner_is_invoked_with_the_assembly(&mut context);
+    then_the_standard_output_should_have(
+        context,
+        "test result: ok. 0 passed; 0 failed; 1 ignored; 0 filtered out",
+    );
+}
