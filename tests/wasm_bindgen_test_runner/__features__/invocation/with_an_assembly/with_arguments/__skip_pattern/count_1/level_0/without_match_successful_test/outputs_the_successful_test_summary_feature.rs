a05190b53c89760f2b20f2fcc4db2d8d0289f851
Alexandre Faria <alexandre.faria@auroka.pt>
2024-04-16 23:34:57 +0100

    wasm-bindgen-test-runner: Added test feature Outputs the successful test summary to the invocation with_an_assembly with_arguments --skip-pattern count_1 level_0 without_match_successful_test.

---
+++ tests/wasm_bindgen_test_runner/__features__/invocation/with_an_assembly/with_arguments/__skip_pattern/count_1/level_0/without_match_successful_test/outputs_the_successful_test_summary_feature.rs
@@ -0,0 +1,18 @@
+use crate::__steps__::assembly::given_there_is_an_assembly_with_one_successful_test;
+use crate::__steps__::standard_output::then_the_standard_output_should_have;
+use crate::__steps__::wasm_bindgen_test_runner::when_wasm_bindgen_test_runner_is_invoked_with_the_assembly_and_the_arguments;
+use crate::__steps__::Context;
+
+#[test]
+fn outputs_the_successful_test_summary_feature() {
+    let mut context = Context::new();
+    given_there_is_an_assembly_with_one_successful_test(&mut context);
+    when_wasm_bindgen_test_runner_is_invoked_with_the_assembly_and_the_arguments(
+        &mut context,
+        "--skip pattern",
+    );
+    then_the_standard_output_should_have(
+        context,
+        "test assembly_with_one_successful_test::pass ... ok",
+    );
+}
