812e4fbe1f30f88ccc880b44c3d82fe9c3920ebf
Alexandre Faria <alexandre.faria@auroka.pt>
2024-04-17 20:43:51 +0100

    wasm-bindgen-test-runner: Added features tests to the invocation with_an_assembly with_arguments --skip PATTERN count_2 level_1 with_two_partial_match_successful_tests.

---
+++ tests/wasm_bindgen_test_runner/__features__/invocation/with_an_assembly/with_arguments/__skip_pattern/count_2/level_1/with_two_partial_match_successful_tests/outputs_no_error_feature.rs
@@ -0,0 +1,15 @@
+use crate::__steps__::assembly::given_there_is_an_assembly_with_two_successful_level_1_tests;
+use crate::__steps__::standard_error::then_the_standard_error_should_be_empty;
+use crate::__steps__::wasm_bindgen_test_runner::when_wasm_bindgen_test_runner_is_invoked_with_the_assembly_and_the_arguments;
+use crate::__steps__::Context;
+
+#[test]
+fn outputs_no_error_feature() {
+    let mut context = Context::new();
+    given_there_is_an_assembly_with_two_successful_level_1_tests(&mut context);
+    when_wasm_bindgen_test_runner_is_invoked_with_the_assembly_and_the_arguments(
+        &mut context,
+        "--skip pass_1 --skip level_1",
+    );
+    then_the_standard_error_should_be_empty(context);
+}
---
+++ tests/wasm_bindgen_test_runner/__features__/invocation/with_an_assembly/with_arguments/__skip_pattern/count_2/level_1/with_two_partial_match_successful_tests/outputs_no_information_about_the_skipped_test_2_feature.rs
@@ -0,0 +1,18 @@
+use crate::__steps__::assembly::given_there_is_an_assembly_with_two_successful_level_1_tests;
+use crate::__steps__::standard_output::then_the_standard_output_should_not_have;
+use crate::__steps__::wasm_bindgen_test_runner::when_wasm_bindgen_test_runner_is_invoked_with_the_assembly_and_the_arguments;
+use crate::__steps__::Context;
+
+#[test]
+fn outputs_no_information_about_the_skipped_test_2_feature() {
+    let mut context = Context::new();
+    given_there_is_an_assembly_with_two_successful_level_1_tests(&mut context);
+    when_wasm_bindgen_test_runner_is_invoked_with_the_assembly_and_the_arguments(
+        &mut context,
+        "--skip pass_1 --skip level_1",
+    );
+    then_the_standard_output_should_not_have(
+        context,
+        "test assembly_with_one_successful_test::pass_1",
+    );
+}
