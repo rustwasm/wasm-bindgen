63666512005ca07543a1e0b58c9b1f32d4c6d787
Alexandre Faria <alexandre.faria@auroka.pt>
2024-04-17 20:28:43 +0100

    wasm-bindgen-test-runner: Added features tests to the invocation with_an_assembly with_arguments --skip PATTERN count_1 level_1.

---
+++ tests/wasm_bindgen_test_runner/__features__/invocation/with_an_assembly/with_arguments/__skip_pattern/count_1/level_1/with_one_full_match_successful_test/outputs_the_assembly_test_summary_feature.rs
@@ -0,0 +1,18 @@
+use crate::__steps__::assembly::given_there_is_an_assembly_with_one_successful_level_1_test;
+use crate::__steps__::standard_output::then_the_standard_output_should_have;
+use crate::__steps__::wasm_bindgen_test_runner::when_wasm_bindgen_test_runner_is_invoked_with_the_assembly_and_the_arguments;
+use crate::__steps__::Context;
+
+#[test]
+fn outputs_the_assembly_test_summary_feature() {
+    let mut context = Context::new();
+    given_there_is_an_assembly_with_one_successful_level_1_test(&mut context);
+    when_wasm_bindgen_test_runner_is_invoked_with_the_assembly_and_the_arguments(
+        &mut context,
+        "--skip level_1::pass",
+    );
+    then_the_standard_output_should_have(
+        context,
+        "test result: ok. 0 passed; 0 failed; 0 ignored; 1 filtered out",
+    );
+}
---
+++ tests/wasm_bindgen_test_runner/__steps__/assembly/with_two_successful_tests/given_there_is_an_assembly_with_two_successful_level_1_tests.rs
@@ -0,0 +1,33 @@
+use super::super::AssemblyBuilder;
+use crate::__steps__::wasm_bindgen_test_runner::Sandbox;
+use crate::__steps__::Context;
+use lazy_static::lazy_static;
+use std::path::PathBuf;
+
+lazy_static! {
+    static ref ASSEMBLY: PathBuf =
+        AssemblyBuilder::new("assembly_with_two_successful_level_1_tests")
+            .file(
+                "src/lib.rs",
+                r#"#[cfg(test)]
+mod level_1 {
+    use wasm_bindgen_test::*;
+
+    #[wasm_bindgen_test]
+    fn pass_1() {
+        assert_eq!(1, 1);
+    }
+
+    #[wasm_bindgen_test]
+    fn pass_2() {
+        assert_eq!(1, 1);
+    }
+}
+"#,
+            )
+            .build();
+}
+
+pub fn given_there_is_an_assembly_with_two_successful_level_1_tests(context: &mut Context) {
+    context.sandbox_set(Sandbox::new(ASSEMBLY.clone()));
+}
