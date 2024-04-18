0927878ce2af0589d8ff4d9f333ec7ce540de80d
Alexandre Faria <alexandre.faria@auroka.pt>
2024-04-18 20:52:11 +0100

    wasm-bindgen-test-runner: Added feature tests for the invocation with_an_assembly with_arguments TESTNAME default with_one_partial_match_successful_test.

---
+++ tests/wasm_bindgen_test_runner/__features__/invocation/with_an_assembly/with_arguments/testname/default/with_one_partial_match_successful_test/outputs_its_running_1_test_feature.rs
@@ -0,0 +1,15 @@
+use crate::__steps__::assembly::given_there_is_an_assembly_with_one_successful_test;
+use crate::__steps__::standard_output::then_the_standard_output_should_have;
+use crate::__steps__::wasm_bindgen_test_runner::when_wasm_bindgen_test_runner_is_invoked_with_the_assembly_and_the_arguments;
+use crate::__steps__::Context;
+
+#[test]
+fn outputs_its_running_1_test_feature() {
+    let mut context = Context::new();
+    given_there_is_an_assembly_with_one_successful_test(&mut context);
+    when_wasm_bindgen_test_runner_is_invoked_with_the_assembly_and_the_arguments(
+        &mut context,
+        "as",
+    );
+    then_the_standard_output_should_have(context, "running 1 test");
+}
