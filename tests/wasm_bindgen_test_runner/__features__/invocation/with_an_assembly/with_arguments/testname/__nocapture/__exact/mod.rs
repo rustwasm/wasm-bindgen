cbbf5863d46aa80393945a76e1fdde05564b360d
Alexandre Faria <alexandre.faria@auroka.pt>
2024-04-20 21:30:32 +0100

    wasm-bindgen-test-runner: Added test features to the invocation with_an_assembly with_arguments TESTNAME --no-capture --exact with_successful_test_match_2_of_2.

--- tests/wasm_bindgen_test_runner/__features__/invocation/with_an_assembly/with_arguments/testname/__nocapture/__exact/mod.rs
+++ tests/wasm_bindgen_test_runner/__features__/invocation/with_an_assembly/with_arguments/testname/__nocapture/__exact/mod.rs
@@ -1,1 +1,2 @@
 mod with_successful_test_match_1_of_2;
+mod with_successful_test_match_2_of_2;
---
+++ tests/wasm_bindgen_test_runner/__features__/invocation/with_an_assembly/with_arguments/testname/__nocapture/__exact/with_successful_test_match_2_of_2/returns_success_feature.rs
@@ -0,0 +1,15 @@
+use crate::__steps__::assembly::given_there_is_an_assembly_with_two_successful_tests;
+use crate::__steps__::success::then_success_should_have_been_returned;
+use crate::__steps__::wasm_bindgen_test_runner::when_wasm_bindgen_test_runner_is_invoked_with_the_assembly_and_the_arguments;
+use crate::__steps__::Context;
+
+#[test]
+fn returns_success_feature() {
+    let mut context = Context::new();
+    given_there_is_an_assembly_with_two_successful_tests(&mut context);
+    when_wasm_bindgen_test_runner_is_invoked_with_the_assembly_and_the_arguments(
+        &mut context,
+        "pass_2 --nocapture --exact",
+    );
+    then_success_should_have_been_returned(context);
+}
