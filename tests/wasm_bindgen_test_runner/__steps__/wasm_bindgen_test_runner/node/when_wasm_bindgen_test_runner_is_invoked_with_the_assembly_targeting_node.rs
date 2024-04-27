ceabe5a307fff7c1dc9f4f3ce4b97ea72cb0e059
Alexandre Faria <alexandre.faria@auroka.pt>
2024-04-27 18:51:34 +0100

    wasm-bindgen-test-runner: Improved test step formatting.

--- tests/wasm_bindgen_test_runner/__steps__/wasm_bindgen_test_runner/node/when_wasm_bindgen_test_runner_is_invoked_with_the_assembly_targeting_node.rs
+++ tests/wasm_bindgen_test_runner/__steps__/wasm_bindgen_test_runner/node/when_wasm_bindgen_test_runner_is_invoked_with_the_assembly_targeting_node.rs
@@ -1,6 +1,8 @@
 use crate::__steps__::{wasm_bindgen_test_runner::wasm_bindgen_test_runner_command, Context};
 
-pub fn when_wasm_bindgen_test_runner_is_invoked_with_the_assembly_targeting_node(context: &mut Context) {
+pub fn when_wasm_bindgen_test_runner_is_invoked_with_the_assembly_targeting_node(
+    context: &mut Context,
+) {
     let mut command = wasm_bindgen_test_runner_command();
 
     command.env("WASM_BINDGEN_TEST_ONLY_NODE", "true");
