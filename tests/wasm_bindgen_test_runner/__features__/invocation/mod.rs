e744d4404cc2847d68e276144351bb7c408d2721
Alexandre Faria <alexandre.faria@auroka.pt>
2024-04-27 16:19:54 +0100

    wasm-bindgen-test-runner: Added feature tests for the invocation runtimes node with_one_successful_test.

--- tests/wasm_bindgen_test_runner/__features__/invocation/mod.rs
+++ tests/wasm_bindgen_test_runner/__features__/invocation/mod.rs
@@ -1,3 +1,4 @@
 mod options;
+mod runtimes;
 mod with_an_assembly;
 mod without_arguments;
--- tests/wasm_bindgen_test_runner/__steps__/wasm_bindgen_test_runner/mod.rs
+++ tests/wasm_bindgen_test_runner/__steps__/wasm_bindgen_test_runner/mod.rs
@@ -1,3 +1,4 @@
+mod node;
 mod sandbox;
 mod wasm_bindgen_test_runner_command;
 mod when_wasm_bindgen_test_runner_is_invoked_with_the_assembly;
@@ -5,6 +6,7 @@ mod when_wasm_bindgen_test_runner_is_invoked_with_the_assembly_and_the_arguments
 mod when_wasm_bindgen_test_runner_is_invoked_with_the_option;
 mod when_wasm_bindgen_test_runner_is_invoked_without_arguments;
 
+pub use node::*;
 pub use sandbox::*;
 pub use wasm_bindgen_test_runner_command::*;
 pub use when_wasm_bindgen_test_runner_is_invoked_with_the_assembly::*;
