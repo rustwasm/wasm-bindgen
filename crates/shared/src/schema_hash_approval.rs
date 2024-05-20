9e3f4c88510d74cc533957cbf11ad37c2e73336c
David Huculak <davidhuculak5@gmail.com>
2024-05-19 22:41:27 -0400

    fix incorrect schema hash after merging from master

--- crates/shared/src/schema_hash_approval.rs
+++ crates/shared/src/schema_hash_approval.rs
@@ -8,7 +8,7 @@
 // If the schema in this library has changed then:
 //  1. Bump the version in `crates/shared/Cargo.toml`
 //  2. Change the `SCHEMA_VERSION` in this library to this new Cargo.toml version
-const APPROVED_SCHEMA_FILE_HASH: &str = "6582330021812361761";
+const APPROVED_SCHEMA_FILE_HASH: &str = "16355783286637101026";
 
 #[test]
 fn schema_version() {
