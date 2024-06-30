24968f8d0e62bdfe37fdb9783dd362b26e3cf104
Alexandre Faria <alexandre.faria@auroka.pt>
2024-06-30 12:41:33 +0100

    wasm-bindgen-test-runner: Improved formatting.

--- tests/wasm_bindgen_test_runner/__steps__/mod.rs
+++ tests/wasm_bindgen_test_runner/__steps__/mod.rs
@@ -1,10 +1,10 @@
 pub mod assembly;
 mod context;
 pub mod error_code;
-mod test_mode;
 pub mod standard_error;
 pub mod standard_output;
 pub mod success;
+mod test_mode;
 pub mod wasm_bindgen_test_runner;
 
 pub use context::*;
