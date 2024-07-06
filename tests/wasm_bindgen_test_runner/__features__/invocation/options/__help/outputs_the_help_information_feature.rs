3ed2f05424a38ad42bd581d2d1e8d0540b941509
Alexandre Faria <alexandre.faria@auroka.pt>
2024-07-07 00:45:23 +0100

    wasm-bindgen-test-runner: Simplified feature tests invocation::options.

--- tests/wasm_bindgen_test_runner/__features__/invocation/options/__help/outputs_the_help_information_feature.rs
+++ tests/wasm_bindgen_test_runner/__features__/invocation/options/__help/outputs_the_help_information_feature.rs
@@ -1,4 +1,3 @@
-use crate::__steps__::assembly::given_there_is_an_assembly_with_one_failing_test;
 use crate::__steps__::standard_output::then_the_standard_output_should_have;
 use crate::__steps__::success::then_success_should_have_been_returned;
 use crate::__steps__::wasm_bindgen_test_runner::when_wasm_bindgen_test_runner_is_invoked_with_the_option;
@@ -6,7 +5,6 @@ use crate::__steps__::Context;
 use auroka_morpheus_macros_feature::feature;
 
 feature! {
-    given_there_is_an_assembly_with_one_failing_test();
     when_wasm_bindgen_test_runner_is_invoked_with_the_option("--help");
 
     "Outputs the wasm-bindgen-test-runner help information" {
