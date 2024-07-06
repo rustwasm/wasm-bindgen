3620c99f525726f7b10d50882cd727f528b7df09
Alexandre Faria <alexandre.faria@auroka.pt>
2024-07-06 23:09:26 +0100

    wasm-bindgen-test-runner: Simplified feature test for invocation without arguments.

--- tests/wasm_bindgen_test_runner/__features__/invocation/without_arguments/handles_missing_arguments_feature.rs
+++ tests/wasm_bindgen_test_runner/__features__/invocation/without_arguments/handles_missing_arguments_feature.rs
@@ -1,4 +1,3 @@
-use crate::__steps__::assembly::given_there_is_an_assembly_with_one_failing_test;
 use crate::__steps__::error_code::then_failure_should_have_been_returned;
 use crate::__steps__::standard_error::then_the_standard_error_should_have;
 use crate::__steps__::wasm_bindgen_test_runner::when_wasm_bindgen_test_runner_is_invoked_without_arguments;
@@ -6,7 +5,6 @@ use crate::__steps__::Context;
 use auroka_morpheus_macros_feature::feature;
 
 feature! {
-    given_there_is_an_assembly_with_one_failing_test();
     when_wasm_bindgen_test_runner_is_invoked_without_arguments();
 
     "Outputs the wasm-bindgen-test-runner usage information" {
