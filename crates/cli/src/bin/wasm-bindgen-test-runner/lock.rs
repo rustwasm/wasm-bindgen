4d93a2ee8f32db828862dee1e553db3ff774bf6c
Alexandre Faria <alexandre.faria@auroka.pt>
2024-07-09 00:38:10 +0100

    wasm-bindgen-test-runner: Improved Lock: Added an anti-starvation mechanism.

--- crates/cli/src/bin/wasm-bindgen-test-runner/lock.rs
+++ crates/cli/src/bin/wasm-bindgen-test-runner/lock.rs
@@ -1,21 +1,29 @@
 use anyhow::Result;
-use std::fs::{hard_link, File, OpenOptions};
+use std::fs::{hard_link, metadata, read_dir, DirEntry, File, OpenOptions};
+use std::io::Error;
 use std::io::{Read, Write};
 use std::path::PathBuf;
 use std::process::{id, Command};
 use std::time::Duration;
+use std::time::SystemTime;
 use std::{env, thread};
 
 pub struct Lock {
     file: PathBuf,
     lock: PathBuf,
+    name: String,
+    timestamp: SystemTime,
 }
 
 impl Lock {
     pub fn try_new(name: &str) -> Result<Self> {
+        let file = Self::create_file(name)?;
+        let timestamp = metadata(&file).unwrap().modified().unwrap();
         let mut lock = Self {
-            file: Self::create_file(name)?,
+            file,
             lock: Self::create_lock(name)?,
+            name: name.to_string(),
+            timestamp,
         };
         lock.aquire()?;
         Ok(lock)
@@ -58,6 +66,46 @@ impl Lock {
         false
     }
 
+    fn is_oldest_waiting(&self) -> bool {
+        let lock_name = format!("{}.lock", &self.name);
+        let file_prefix = format!("{}.", &self.name);
+
+        for entry in read_dir(env::temp_dir()).unwrap() {
+            if let Some((path, name)) = Self::is_candidate(entry, &lock_name, &file_prefix) {
+                if self.is_candidate_older(&path) && Self::is_candidate_running(&name) {
+                    return false;
+                }
+            }
+        }
+
+        true
+    }
+
+    fn is_candidate(
+        entry: Result<DirEntry, Error>,
+        lock_name: &str,
+        file_prefix: &str,
+    ) -> Option<(PathBuf, String)> {
+        if let Ok(entry) = entry {
+            let path = entry.path();
+            if let Some(name) = path.clone().file_name().and_then(|n| n.to_str()) {
+                if name.starts_with(file_prefix) && name != lock_name {
+                    return Some((path, name.to_string()));
+                }
+            }
+        }
+        None
+    }
+
+    fn is_candidate_older(&self, candidate: &PathBuf) -> bool {
+        metadata(candidate).unwrap().modified().unwrap() < self.timestamp
+    }
+
+    fn is_candidate_running(name: &str) -> bool {
+        let pid = name.split('.').last().unwrap().parse().unwrap();
+        is_process_running(pid)
+    }
+
     fn read_lock_pid(&self) -> Result<u32> {
         let mut file = File::open(&self.lock)?;
         let mut pid = String::new();
@@ -85,6 +133,10 @@ impl Lock {
             }
 
             self.remove_lock();
+        }
+
+        if !self.is_oldest_waiting() {
+            return Ok(false);
         }
 
         hard_link(&self.file, &self.lock).ok();
