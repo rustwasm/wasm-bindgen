76f9e72ba58db0196cee0edc859eb338d3e3f4dd
Alexandre Faria <alexandre.faria@auroka.pt>
2024-07-10 21:14:05 +0100

    wasm-bindgen-test-runner: Simplified feature test step implementation.

--- tests/wasm_bindgen_test_runner/__steps__/assembly/given_there_is_an_assembly_without_anything.rs
+++ tests/wasm_bindgen_test_runner/__steps__/assembly/given_there_is_an_assembly_without_anything.rs
@@ -1,19 +1,6 @@
-use super::AssemblyBuilder;
-use crate::__steps__::wasm_bindgen_test_runner::Sandbox;
+use crate::__steps__::assembly::given_there_is_an_assembly_with;
 use crate::__steps__::Context;
-use lazy_static::lazy_static;
-use std::path::PathBuf;
-
-lazy_static! {
-    static ref ASSEMBLY: PathBuf = AssemblyBuilder::new("assembly_without_anything")
-        .file(
-            "src/lib.rs",
-            r#"
-            "#,
-        )
-        .build();
-}
 
 pub fn given_there_is_an_assembly_without_anything(context: &mut Context) {
-    context.sandbox_set(Sandbox::new(ASSEMBLY.clone()));
+    given_there_is_an_assembly_with(context, "");
 }
