b1f9d109e8d8dd81ae611edf23cc37ce1b89fc35
Alexandre Faria <alexandre.faria@auroka.pt>
2024-07-10 19:52:17 +0100

    wasm-bindgen-test-runner: Updated feature tests to use a more explicit step in terms of the content of the assembly.

--- tests/wasm_bindgen_test_runner/__steps__/assembly/mod.rs
+++ tests/wasm_bindgen_test_runner/__steps__/assembly/mod.rs
@@ -4,7 +4,6 @@ mod given_there_is_an_assembly_without_anything;
 mod with_one_ignored_test;
 mod with_one_successful_and_one_failing_tests;
 mod with_one_successful_and_one_ignored_tests;
-mod with_one_successful_test;
 mod with_two_successful_tests;
 
 pub use assembly_builder::*;
@@ -13,5 +12,4 @@ pub use given_there_is_an_assembly_without_anything::*;
 pub use with_one_ignored_test::*;
 pub use with_one_successful_and_one_failing_tests::*;
 pub use with_one_successful_and_one_ignored_tests::*;
-pub use with_one_successful_test::*;
 pub use with_two_successful_tests::*;
