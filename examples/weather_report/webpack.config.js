2ea6ab1c9402ac13c361d7cfe9818bea006cf9bd
Chris <me@systemcluster.me>
2024-08-07 00:21:40 +0200

    Improve the CI runtime (#4056)

--- examples/weather_report/webpack.config.js
+++ examples/weather_report/webpack.config.js
@@ -16,12 +16,6 @@ module.exports = {
         new WasmPackPlugin({
             crateDirectory: path.resolve(__dirname, ".")
         }),
-        // Have this example work in Edge which doesn't ship `TextEncoder` or
-        // `TextDecoder` at this time.
-        new webpack.ProvidePlugin({
-            TextDecoder: ['text-encoding', 'TextDecoder'],
-            TextEncoder: ['text-encoding', 'TextEncoder']
-        })
     ],
     mode: 'development',
     experiments: {
