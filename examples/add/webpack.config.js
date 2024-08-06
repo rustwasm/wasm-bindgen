106009c6e3c82625e830844e2e59114f2d33421d
Systemcluster <me@systemcluster.me>
2024-08-06 19:34:23 +0200

    Remove text-encoding polyfill
    It is marked deprecated and unnecessary today: https://www.npmjs.com/package/text-encoding

--- examples/add/webpack.config.js
+++ examples/add/webpack.config.js
@@ -14,12 +14,6 @@ module.exports = {
         new WasmPackPlugin({
             crateDirectory: path.resolve(__dirname, ".")
         }),
-        // Have this example work in Edge which doesn't ship `TextEncoder` or
-        // `TextDecoder` at this time.
-        new webpack.ProvidePlugin({
-          TextDecoder: ['text-encoding', 'TextDecoder'],
-          TextEncoder: ['text-encoding', 'TextEncoder']
-        })
     ],
     mode: 'development',
     experiments: {
--- examples/todomvc/webpack.config.js
+++ examples/todomvc/webpack.config.js
@@ -18,12 +18,6 @@ module.exports = {
         new WasmPackPlugin({
             crateDirectory: path.resolve(__dirname, ".")
         }),
-        // Have this example work in Edge which doesn't ship `TextEncoder` or
-        // `TextDecoder` at this time.
-        new webpack.ProvidePlugin({
-          TextDecoder: ['text-encoding', 'TextDecoder'],
-          TextEncoder: ['text-encoding', 'TextEncoder']
-        })
     ],
     mode: 'development',
     experiments: {
