0be8fb6e7993b654b07a7747c8d9f88861293baa
RunDevelopment <mitchi5000.ms@googlemail.com>
2024-10-08 16:15:39 +0200

    Update schema hash

--- crates/shared/src/schema_hash_approval.rs
+++ crates/shared/src/schema_hash_approval.rs
@@ -8,7 +8,7 @@
 // If the schema in this library has changed then:
 //  1. Bump the version in `crates/shared/Cargo.toml`
 //  2. Change the `SCHEMA_VERSION` in this library to this new Cargo.toml version
-const APPROVED_SCHEMA_FILE_HASH: &str = "17362973054744490747";
+const APPROVED_SCHEMA_FILE_HASH: &str = "9179028021460341559";
 
 #[test]
 fn schema_version() {
