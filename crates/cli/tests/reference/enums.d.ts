96af02d5e13d6cd969b2c1b944e76736e1bdb3a9
RunDevelopment <mitchi5000.ms@googlemail.com>
2024-10-12 23:59:30 +0200

    Update refs

--- crates/cli/tests/reference/enums.d.ts
+++ crates/cli/tests/reference/enums.d.ts
@@ -2,8 +2,8 @@
 /* eslint-disable */
 export function enum_echo(color: Color): Color;
 export function option_enum_echo(color?: Color): Color | undefined;
-export function get_name(color: Color): any;
-export function option_string_enum_echo(color?: any): any | undefined;
+export function get_name(color: Color): ColorName;
+export function option_string_enum_echo(color?: ColorName): ColorName | undefined;
 /**
  * A color.
  */
