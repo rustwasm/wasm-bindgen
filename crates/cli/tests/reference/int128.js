b808ba30017bcf6c90ba431f6e622fdda31e2f7a
Michael Schmidt <mitchi5000.ms@googlemail.com>
2024-11-02 11:42:07 +0100

    Updated refs

--- crates/cli/tests/reference/int128.js
+++ crates/cli/tests/reference/int128.js
@@ -3,29 +3,13 @@ export function __wbg_set_wasm(val) {
     wasm = val;
 }
 
-
-let cachedDataViewMemory0 = null;
-
-function getDataViewMemory0() {
-    if (cachedDataViewMemory0 === null || cachedDataViewMemory0.buffer.detached === true || (cachedDataViewMemory0.buffer.detached === undefined && cachedDataViewMemory0.buffer !== wasm.memory.buffer)) {
-        cachedDataViewMemory0 = new DataView(wasm.memory.buffer);
-    }
-    return cachedDataViewMemory0;
-}
 /**
  * @param {bigint} a
  * @returns {bigint}
  */
 export function echo_i128(a) {
-    try {
-        const retptr = wasm.__wbindgen_add_to_stack_pointer(-16);
-        wasm.echo_i128(retptr, a, a >> 64n);
-        var r0 = getDataViewMemory0().getBigInt64(retptr + 8 * 0, true);
-        var r2 = getDataViewMemory0().getBigInt64(retptr + 8 * 1, true);
-        return (BigInt.asUintN(64, r0) | (r2 << 64n));
-    } finally {
-        wasm.__wbindgen_add_to_stack_pointer(16);
-    }
+    const ret = wasm.echo_i128(a, a >> 64n);
+    return (BigInt.asUintN(64, ret[0]) | (ret[1] << 64n));
 }
 
 /**
@@ -33,15 +17,8 @@ export function echo_i128(a) {
  * @returns {bigint}
  */
 export function echo_u128(a) {
-    try {
-        const retptr = wasm.__wbindgen_add_to_stack_pointer(-16);
-        wasm.echo_u128(retptr, a, a >> 64n);
-        var r0 = getDataViewMemory0().getBigInt64(retptr + 8 * 0, true);
-        var r2 = getDataViewMemory0().getBigInt64(retptr + 8 * 1, true);
-        return (BigInt.asUintN(64, r0) | (BigInt.asUintN(64, r2) << 64n));
-    } finally {
-        wasm.__wbindgen_add_to_stack_pointer(16);
-    }
+    const ret = wasm.echo_u128(a, a >> 64n);
+    return (BigInt.asUintN(64, ret[0]) | (BigInt.asUintN(64, ret[1]) << 64n));
 }
 
 function isLikeNone(x) {
@@ -52,16 +29,8 @@ function isLikeNone(x) {
  * @returns {bigint | undefined}
  */
 export function echo_option_i128(a) {
-    try {
-        const retptr = wasm.__wbindgen_add_to_stack_pointer(-32);
-        wasm.echo_option_i128(retptr, !isLikeNone(a), isLikeNone(a) ? 0n : a, isLikeNone(a) ? 0n : a >> 64n);
-        var r0 = getDataViewMemory0().getInt32(retptr + 4 * 0, true);
-        var r2 = getDataViewMemory0().getBigInt64(retptr + 8 * 1, true);
-        var r4 = getDataViewMemory0().getBigInt64(retptr + 8 * 2, true);
-        return r0 === 0 ? undefined : (BigInt.asUintN(64, r2) | (r4 << 64n));
-    } finally {
-        wasm.__wbindgen_add_to_stack_pointer(32);
-    }
+    const ret = wasm.echo_option_i128(!isLikeNone(a), isLikeNone(a) ? 0n : a, isLikeNone(a) ? 0n : a >> 64n);
+    return ret[0] === 0 ? undefined : (BigInt.asUintN(64, ret[1]) | (ret[2] << 64n));
 }
 
 /**
@@ -69,16 +38,8 @@ export function echo_option_i128(a) {
  * @returns {bigint | undefined}
  */
 export function echo_option_u128(a) {
-    try {
-        const retptr = wasm.__wbindgen_add_to_stack_pointer(-32);
-        wasm.echo_option_u128(retptr, !isLikeNone(a), isLikeNone(a) ? 0n : a, isLikeNone(a) ? 0n : a >> 64n);
-        var r0 = getDataViewMemory0().getInt32(retptr + 4 * 0, true);
-        var r2 = getDataViewMemory0().getBigInt64(retptr + 8 * 1, true);
-        var r4 = getDataViewMemory0().getBigInt64(retptr + 8 * 2, true);
-        return r0 === 0 ? undefined : (BigInt.asUintN(64, r2) | (BigInt.asUintN(64, r4) << 64n));
-    } finally {
-        wasm.__wbindgen_add_to_stack_pointer(32);
-    }
+    const ret = wasm.echo_option_u128(!isLikeNone(a), isLikeNone(a) ? 0n : a, isLikeNone(a) ? 0n : a >> 64n);
+    return ret[0] === 0 ? undefined : (BigInt.asUintN(64, ret[1]) | (BigInt.asUintN(64, ret[2]) << 64n));
 }
 
 const heap = new Array(128).fill(undefined);
@@ -104,19 +65,10 @@ function takeObject(idx) {
  * @returns {bigint}
  */
 export function throw_i128() {
-    try {
-        const retptr = wasm.__wbindgen_add_to_stack_pointer(-32);
-        wasm.throw_i128(retptr);
-        var r0 = getDataViewMemory0().getBigInt64(retptr + 8 * 0, true);
-        var r2 = getDataViewMemory0().getBigInt64(retptr + 8 * 1, true);
-        var r4 = getDataViewMemory0().getInt32(retptr + 4 * 4, true);
-        var r5 = getDataViewMemory0().getInt32(retptr + 4 * 5, true);
-        if (r5) {
-            throw takeObject(r4);
-        }
-        return (BigInt.asUintN(64, r0) | (r2 << 64n));
-    } finally {
-        wasm.__wbindgen_add_to_stack_pointer(32);
+    const ret = wasm.throw_i128();
+    if (ret[3]) {
+        throw takeObject(ret[2]);
     }
+    return (BigInt.asUintN(64, ret[0]) | (ret[1] << 64n));
 }
 
