e3de77d77b6e0162c1a4a8b4b06d45f89c998f97
RunDevelopment <mitchi5000.ms@googlemail.com>
2024-11-13 15:08:38 +0100

    Updated test

--- crates/cli/tests/reference/modules.rs
+++ crates/cli/tests/reference/modules.rs
@@ -20,8 +20,19 @@ mod b {
     }
 }
 
+mod a_again {
+    use wasm_bindgen::prelude::*;
+
+    #[wasm_bindgen]
+    extern "C" {
+        #[wasm_bindgen(js_name = parseFloat)]
+        pub fn parse_float(text: &JsValue) -> f64;
+    }
+}
+
 #[wasm_bindgen]
 pub fn exported() {
     let _ = a::parse_float(&JsValue::from_str("3.14"));
     let _ = b::parse_float("3.14");
+    let _ = a_again::parse_float(&JsValue::from_str("3.14"));
 }
