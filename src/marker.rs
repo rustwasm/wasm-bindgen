f07b885edf43f2210f04e1282441e2184d580f3c
RunDevelopment <mitchi5000.ms@googlemail.com>
2024-11-29 17:06:46 +0100

    Add `rustversion` and `diagnostic` feature

--- src/marker.rs
+++ src/marker.rs
@@ -6,6 +6,9 @@
 //! This is an internal module, no stability guarantees are provided. Use at
 //! your own risk.
 
+#[cfg(feature = "diagnostic")]
+extern crate rustversion;
+
 /// Marker trait for types that support `#[wasm_bindgen(constructor)]`.
 ///
 /// **DO NOT** implement this trait manually. It is implemented automatically
@@ -16,11 +19,17 @@
 /// This is part of the internal [`convert`](crate::marker) module, **no
 /// stability guarantees** are provided. Use at your own risk. See its
 /// documentation for more details.
-#[diagnostic::on_unimplemented(
-    message = "JavaScript constructors are not supported for `{Self}`",
-    label = "this function cannot be the constructor of `{Self}`",
-    note = "`#[wasm_bindgen(constructor)]` is generally only supported for `struct`s with `#[wasm_bindgen]` and cannot be used for `enum`s.",
-    note = "Consider removing the `constructor` option and using a regular static method instead."
+#[cfg_attr(
+    feature = "diagnostic",
+    rustversion::attr(
+        since(1.78),
+        diagnostic::on_unimplemented(
+            message = "JavaScript constructors are not supported for `{Self}`",
+            label = "this function cannot be the constructor of `{Self}`",
+            note = "`#[wasm_bindgen(constructor)]` is generally only supported for `struct`s with `#[wasm_bindgen]` and cannot be used for `enum`s.",
+            note = "Consider removing the `constructor` option and using a regular static method instead."
+        )
+    )
 )]
 pub trait SupportsConstructor {}
 
@@ -35,10 +44,16 @@ pub trait SupportsConstructor {}
 /// This is part of the internal [`convert`](crate::marker) module, **no
 /// stability guarantees** are provided. Use at your own risk. See its
 /// documentation for more details.
-#[diagnostic::on_unimplemented(
-    message = "JavaScript instance getters and setters are not supported for `{Self}`",
-    label = "this method cannot be a getter or setter for `{Self}`",
-    note = "`#[wasm_bindgen(getter)]` and `#[wasm_bindgen(setter)]` are generally only supported for `struct`s with `#[wasm_bindgen]`. They cannot be used for `enum`s."
+#[cfg_attr(
+    feature = "diagnostic",
+    rustversion::attr(
+        since(1.78),
+        diagnostic::on_unimplemented(
+            message = "JavaScript instance getters and setters are not supported for `{Self}`",
+            label = "this method cannot be a getter or setter for `{Self}`",
+            note = "`#[wasm_bindgen(getter)]` and `#[wasm_bindgen(setter)]` are generally only supported for `struct`s with `#[wasm_bindgen]`. They cannot be used for `enum`s."
+        )
+    )
 )]
 pub trait SupportsInstanceProperty {}
 
@@ -53,9 +68,15 @@ pub trait SupportsInstanceProperty {}
 /// This is part of the internal [`convert`](crate::marker) module, **no
 /// stability guarantees** are provided. Use at your own risk. See its
 /// documentation for more details.
-#[diagnostic::on_unimplemented(
-    message = "JavaScript static getters and setters are not supported for `{Self}`",
-    label = "this static function cannot be a static getter or setter on `{Self}`",
-    note = "`#[wasm_bindgen(getter)]` and `#[wasm_bindgen(setter)]` are generally only supported for `struct`s with `#[wasm_bindgen]`. They cannot be used for `enum`s."
+#[cfg_attr(
+    feature = "diagnostic",
+    rustversion::attr(
+        since(1.78),
+        diagnostic::on_unimplemented(
+            message = "JavaScript static getters and setters are not supported for `{Self}`",
+            label = "this static function cannot be a static getter or setter on `{Self}`",
+            note = "`#[wasm_bindgen(getter)]` and `#[wasm_bindgen(setter)]` are generally only supported for `struct`s with `#[wasm_bindgen]`. They cannot be used for `enum`s."
+        )
+    )
 )]
 pub trait SupportsStaticProperty {}
