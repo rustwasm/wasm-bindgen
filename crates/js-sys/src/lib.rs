b1130479100422f114f5b728bb942f6bddc037c3
Paul Hansen <mail@paul.rs>
2025-01-04 14:17:33 -0600

    cargo fmt

--- crates/js-sys/src/lib.rs
+++ crates/js-sys/src/lib.rs
@@ -3393,7 +3393,11 @@ extern "C" {
     pub fn to_locale_time_string(this: &Date, locale: &str) -> JsString;
 
     #[wasm_bindgen(method, js_name = toLocaleTimeString)]
-    pub fn to_locale_time_string_with_options(this: &Date, locale: &str, options: &JsValue) -> JsString;
+    pub fn to_locale_time_string_with_options(
+        this: &Date,
+        locale: &str,
+        options: &JsValue,
+    ) -> JsString;
 
     /// The `toString()` method returns a string representing
     /// the specified Date object.
